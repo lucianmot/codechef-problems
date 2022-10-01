b07c3926d3c88cc2375f10bd8545b332c60d29e1
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-01 20:51:26 +0100

    added solution design for 1 problem

---
+++ C++ - Make them equal.cpp
@@ -0,0 +1,15 @@
+// C++ - Make them equal
+// Rating - ---
+// https://www.codechef.com/LP1TO202/problems/MAKEEQUAL
+
+/*
+Solution design
+take in testcases
+take in array length
+create a min element equal to 100.000.000
+create a max element equal to like 0
+take in array elements one by one with cin
+if statements if smaller than min replace it
+if larger than max replace it
+do difference between min and max and cout
+*/
