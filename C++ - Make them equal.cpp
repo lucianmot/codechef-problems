43edab0cb53364542e7112adb1ca80f32ff4a5da
lucian <37814580+lucianmot@users.noreply.github.com>
2022-10-01 21:54:19 +0100

    Update C++ - Make them equal.cpp

--- C++ - Make them equal.cpp
+++ C++ - Make them equal.cpp
@@ -10,6 +10,6 @@ create a min element equal to 100.000.000
 create a max element equal to like 0
 take in array elements one by one with cin
 if statements if smaller than min replace it
-if larger than max replace it
+if larger than max replace it with value
 do difference between min and max and cout
 */
