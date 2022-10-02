3e7329f4ff3f7f30c7c7873eab04d98771821733
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-02 11:24:34 +0100

    implement solution design from yesterday

--- C++ - Make them equal.cpp
+++ C++ - Make them equal.cpp
@@ -2,14 +2,30 @@
 // Rating - ---
 // https://www.codechef.com/LP1TO202/problems/MAKEEQUAL
 
-/*
-Solution design
-take in testcases
-take in array length
-create a min element equal to 100.000.000
-create a max element equal to like 0
-take in array elements one by one with cin
-if statements if smaller than min replace it
-if larger than max replace it
-do difference between min and max and cout
-*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    int n;
+	    cin >> n;
+	    int min = 1000000000;
+	    int max = 0;
+	    int aList[n];
+	    for ( int i = 0; i < n; i++ ) {
+	        int num;
+	        cin >> num;
+	        if ( num < min ) {
+	            min = num;
+	        }
+	        if ( num > max ) {
+	            max = num;
+	        }
+	        aList[i] = num;
+	    }
+	    cout << max - min << endl;
+	}
+	return 0;
+}
