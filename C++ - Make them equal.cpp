44e973ab95b21b991753082e2e3908f818fe80a7
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-02 11:30:18 +0100

    added new solutio without static array

--- C++ - Make them equal.cpp
+++ C++ - Make them equal.cpp
@@ -2,7 +2,6 @@
 // Rating - ---
 // https://www.codechef.com/LP1TO202/problems/MAKEEQUAL
 
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,7 +13,6 @@ int main() {
 	    cin >> n;
 	    int min = 1000000000;
 	    int max = 0;
-	    int aList[n];
 	    for ( int i = 0; i < n; i++ ) {
 	        int num;
 	        cin >> num;
@@ -24,22 +22,8 @@ int main() {
 	        if ( num > max ) {
 	            max = num;
 	        }
-	        aList[i] = num;
 	    }
 	    cout << max - min << endl;
 	}
 	return 0;
 }
-=======
-/*
-Solution design
-take in testcases
-take in array length
-create a min element equal to 100.000.000
-create a max element equal to like 0
-take in array elements one by one with cin
-if statements if smaller than min replace it
-if larger than max replace it with value
-do difference between min and max and cout
-*/
->>>>>>> 43edab0cb53364542e7112adb1ca80f32ff4a5da
