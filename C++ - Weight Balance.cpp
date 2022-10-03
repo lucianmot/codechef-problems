be15fe2dd4430e2860a0f8e70533ddfe1f7893e9
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-03 22:25:14 +0100

    still couldn't figure it out

--- C++ - Weight Balance.cpp
+++ C++ - Weight Balance.cpp
@@ -19,6 +19,7 @@ int main() {
 	    if (( fat == m * x1 ) || ( fat == m * x2 )) {
 	        cout << "1" << endl;
 	    } else {
+		// something is not right here
 	        for ( int i = 0; i < m; i++ ) {
 	            if ( i % x2 == 0 ) {
 	                fat-=x2;
