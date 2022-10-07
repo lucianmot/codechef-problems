e56bcf49ab1323c0e12978122e04ea11a51c8b37
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-07 13:48:28 +0100

    solved a contest problem

---
+++ C++ - Podium Finish.cpp
@@ -0,0 +1,17 @@
+// C++ - Podium Finish
+// Rating - ---
+// https://www.codechef.com/OCT221D/problems/PODIUM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    int a, b;
+	    cin >> a >> b;
+	    cout << a + b << endl;
+	}
+	return 0;
+}
