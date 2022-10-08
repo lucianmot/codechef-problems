44fdd3e685c16b688f0cb38edc099a3c0a2a87b1
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-08 23:53:51 +0100

    solved easy problem

---
+++ C++ - First and Last Digit.cpp
@@ -0,0 +1,34 @@
+// C++ - First and Last Digit
+// Rating - ---
+// https://www.codechef.com/LP0TO101/problems/FLOW004
+
+// Want to optimise and redo
+
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+#define el endl;
+
+int main()
+{
+   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int n;
+       cin>>n;
+       int rem = n%10;
+       while(n>9)
+       {
+           n = n/10;
+       }
+       cout<<n+rem<<el;
+   }
+   
+return 0;
+
+}
