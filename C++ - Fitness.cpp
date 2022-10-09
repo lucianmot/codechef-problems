3ffb4c10ec86383c0f949f255b95a9160db62c1a
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-09 13:40:21 +0100

    solved another problem

---
+++ C++ - Fitness.cpp
@@ -0,0 +1,17 @@
+// C++ - Fitness
+// Rating - 258
+// https://www.codechef.com/submit/FIT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    int km;
+	    cin >> km;
+	    cout << km * 10 << endl;
+	}
+	return 0;
+}
