7b95086418191821b2fdb3f78a7574d76a232c2e
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-09 14:23:52 +0100

    solved a idiot problem

---
+++ C++ - How many unattempted problems.cpp
@@ -0,0 +1,13 @@
+// C++ - How many unattempted problems
+// Rating - 264
+// https://www.codechef.com/submit/PRACLIST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	    int x, y;
+	    cin >> x >> y;
+	    cout << x - y << endl;
+	return 0;
+}
