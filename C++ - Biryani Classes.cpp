10c74774796e4b16bfe2ad420dbd2293607206e4
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-09 14:01:18 +0100

    solved a easy problem

---
+++ C++ - Biryani Classes.cpp
@@ -0,0 +1,17 @@
+// C++ - Biryani Classes
+// Rating - 257
+// https://www.codechef.com/submit/BIRYANI
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    int x, y;
+	    cin >> x >> y;
+	    cout << x * y << endl;
+	}
+	return 0;
+}
