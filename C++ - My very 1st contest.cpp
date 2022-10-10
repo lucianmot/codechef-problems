4203be384781ceae3d23b943c911983a0bf7c4ad
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-10 20:46:39 +0100

    solved a problem

---
+++ C++ - My very 1st contest.cpp
@@ -0,0 +1,14 @@
+// C++ - My very 1st contest
+// Rating - 284
+// https://www.codechef.com/submit/MY1STCONTEST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, a, b;
+	cin >> n >> a >> b;
+	int m = n - a;
+	cout << m << " " << m - b << endl;
+	return 0;
+}
