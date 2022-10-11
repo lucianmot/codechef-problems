507b99eb7aab5a34bbfe27f54bdf8c76db1d8d00
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-11 20:55:35 +0100

    solved a problem

---
+++ 296 - C++ - Counting Words.cpp
@@ -0,0 +1,16 @@
+// C++ - Counting Words
+// Rating - 296
+// https://www.codechef.com/submit/CNTWRD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    cout << n * m << endl;
+	}
+	return 0;
+}
