acdac8d03f695561dab41bd143de6f954def0dd8
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-11 08:32:02 +0100

    solved a problem

---
+++ 296 - C++ - Total Prize Money.cpp
@@ -0,0 +1,16 @@
+// C++ - Total Prize Money
+// Rating - 296
+// https://www.codechef.com/submit/PRIZEPOOL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << x * 10 + y * 90 << endl;
+	}
+	return 0;
+}
