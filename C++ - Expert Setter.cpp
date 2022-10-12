266d44e16d04f8bbf8041bdb0542f8d9a2380352
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-12 17:23:50 +0100

    solved contest problem

---
+++ C++ - Expert Setter.cpp
@@ -0,0 +1,17 @@
+// C++ - Expert Setter
+// Rating - ---
+// https://www.codechef.com/START60D/problems/EXPERT?tab=submissions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    double half = x / 2.0;
+	    double(y) >= half ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
