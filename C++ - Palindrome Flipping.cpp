64a063e13fac544ed1045049ec12fb692a930b17
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-12 20:30:57 +0100

    read editorial for what i couldn't solve

--- C++ - Palindrome Flipping.cpp
+++ C++ - Palindrome Flipping.cpp
@@ -2,8 +2,6 @@
 // Rating - ---
 // https://www.codechef.com/START60D/problems/FLIPPAL?tab=statement
 
-// unresolved, failing just one test
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,36 +21,11 @@ int main() {
 	            ones++;
 	        }
 	    }
-	    int n_parity;
-	    int zeros_parity;
-	    int ones_parity;
-	    if ( n % 2 == 0 ) {
-	        n_parity = 0;
-	    } else {
-	        n_parity = 1;
-	    }
-	    if ( zeros % 2 == 0 ) {
-	        zeros_parity = 0;
-	    } else {
-	        zeros_parity = 1;
-	    }
-	    if ( ones % 2 == 0 ) {
-	        ones_parity = 0;
-	    } else {
-	        ones_parity = 1;
-	    }
-	    if ( n == 1 ) {
+	    if (( zeros % 2 == 1) && ( ones % 2 == 1 )) {
 	        cout << "NO" << endl;
-	    } else if (( n == 2 ) && ( zeros == 0 || ones == 0 )) {
-	        cout << "YES" << endl;
-	    } else if (( n_parity == 0 ) && ( zeros_parity == 0) && ( ones_parity == 0 )) {
-	        cout << "YES" << endl;
-	    } else if (( n_parity == 1) && (( zeros_parity == 0 ) || ( ones_parity == 0 ))) {
-	        cout << "YES" << endl;
 	    } else {
-	        cout << "NO" << endl;
+	        cout << "YES" << endl;
 	    }
-	   // cout << "this is zeros: " << zeros << " this is ones: " << ones << endl;
 	}
 	return 0;
 }
