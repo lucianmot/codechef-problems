44bb82640eeb21fbafe33374217f35f92eb40a6d
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-14 22:55:43 +0100

    solved another one

---
+++ 315 - C++ - Get Subscription.cpp
@@ -0,0 +1,16 @@
+// C++ - Get Subscription
+// Rating - 315
+// https://www.codechef.com/submit/SUBSCRIBE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    x > 30 ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
