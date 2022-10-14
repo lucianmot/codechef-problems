9e551cbdaf36d5ceed4c71131af26198e6984596
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-14 22:55:01 +0100

    solved a problem

---
+++ 306 - C++ - Number of  Credits.cpp
@@ -0,0 +1,16 @@
+// C++ - Number of Credits
+// Rating - 306
+// https://www.codechef.com/submit/CREDS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin  >> x >> y >> z;
+	    cout << 4 * x + 2 * y << endl;
+	}
+	return 0;
+}
