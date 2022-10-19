8b19f2c20b204d9855a5f1c051c6354f5b9d741c
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-19 08:34:16 +0100

    solve problem

---
+++ 348 - C++ - Fever.cpp
@@ -0,0 +1,16 @@
+// C++ - Fever
+// Rating - 348
+// https://www.codechef.com/submit/FEVER
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    x > 98 ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
