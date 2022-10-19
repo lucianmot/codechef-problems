49fb8bdb74cc78a125aa623c1e09d7b2389a0102
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-19 19:47:30 +0100

    solved a easy problem

---
+++ 350 - C++ - Chef and Chapters.cpp
@@ -0,0 +1,16 @@
+// C++ - Chef and Chapters
+// Rating - 350
+// https://www.codechef.com/submit/SEMCOURSES
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    cout << x * y * z << endl;
+	}
+	return 0;
+}
