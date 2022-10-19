e49d9b734158fe936b83cc27bd9871a3288dec1d
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-19 19:50:41 +0100

    solve problem

---
+++ 351 - C++ - Water Requirement.cpp
@@ -0,0 +1,16 @@
+// C++ - Water Requirement
+// Rating - 351
+// https://www.codechef.com/submit/WATERREQ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    cout << n * 2 << endl;
+	}
+	return 0;
+}
