e6192ed924eef823c28d978766d08123c5da93bc
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-20 21:22:05 +0100

    solved problem

---
+++ 1009 - C++ - Zeros Ones Equal One Zeros.cpp
@@ -0,0 +1,13 @@
+// C++ - Zeros Ones Equal Ones Zeros
+// Rating - 1009
+// https://www.codechef.com/submit/ZOOZ
+
+// Unresolved to be continued
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	return 0;
+}
