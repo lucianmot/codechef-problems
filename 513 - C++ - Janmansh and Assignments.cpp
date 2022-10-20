4cffd6deabbdded8684dbf7e2bb3a47683118c2f
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-20 21:15:31 +0100

    solved problems

---
+++ 513 - C++ - Janmansh and Assignments.cpp
@@ -0,0 +1,16 @@
+// C++ - Janmansh and Assignments.cpp
+// Rating - 513
+// https://www.codechef.com/submit/JASSIGNMENTS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    x + 3 <= 10 ? cout << "Yes" << endl : cout << "No" << endl;
+	}
+	return 0;
+}
