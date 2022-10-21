cb0409b50750ebe922ba5a12f6254509ea564f9a
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-21 23:43:03 +0100

    solved problem

---
+++ 362 - C++ - Alice and Marks.cpp
@@ -0,0 +1,17 @@
+// C++ - Alice and Marks
+// Rating - 362
+// https://www.codechef.com/submit/MARKSTW
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	if ( x >= y + y ) {
+	    cout << "Yes" << endl;
+	} else {
+	    cout << "No" << endl;
+	}
+	return 0;
+}
