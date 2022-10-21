0c9c965c68adc6a879074b64d8fffc29fad66fda
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-21 23:12:55 +0100

    solve problem

---
+++ 355 - C++ - Apples and Oranges.cpp
@@ -0,0 +1,13 @@
+// C++ - Apples and Oranges
+// Rating - 355
+// https://www.codechef.com/submit/APPLORNG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, a, b;
+	cin >> x >> a >> b;
+	x >= a + b ? cout << "Yes" << endl : cout << "No" << endl;
+	return 0;
+}
