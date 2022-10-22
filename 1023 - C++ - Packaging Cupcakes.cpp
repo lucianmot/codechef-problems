1192a100f794b752f6e33e82156e414d5d9b508b
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-22 23:16:27 +0100

    problem on the stack

---
+++ 1023 - C++ - Packaging Cupcakes.cpp
@@ -0,0 +1,15 @@
+// C++ - Packaging Cupcakes
+// Rating - 1023
+// https://www.codechef.com/submit/MUFFINS3
+
+// Unresolved, to be continued
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+	// your code goes here
+	return 0;
+}
