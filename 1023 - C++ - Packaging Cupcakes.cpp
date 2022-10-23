149a158e8b04cf9fe63fd1f3ad99290917812da0
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-23 10:24:40 +0100

    solved problem with hint

--- 1023 - C++ - Packaging Cupcakes.cpp
+++ 1023 - C++ - Packaging Cupcakes.cpp
@@ -2,14 +2,18 @@
 // Rating - 1023
 // https://www.codechef.com/submit/MUFFINS3
 
-// Unresolved, to be continued
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-	// your code goes here
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = n / 2 + 1;
+	    cout << answer << endl;
+	}
 	return 0;
 }
