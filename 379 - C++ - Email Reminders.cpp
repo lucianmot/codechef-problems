80fdc22b4196824759bb93107bd4a364bffd2a2a
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-23 20:43:03 +0100

    solved problem

---
+++ 379 - C++ - Email Reminders.cpp
@@ -0,0 +1,13 @@
+// C++ - Email Reminders
+// Rating - 379
+// https://www.codechef.com/submit/EMAILREM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int u, n;
+	cin >> u >> n;
+	cout << u - n << endl;
+	return 0;
+}
