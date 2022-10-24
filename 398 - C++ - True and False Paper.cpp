848f9267d21c468d58034f474a195d065e91c739
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-24 23:34:57 +0100

    solved problem

---
+++ 398 - C++ - True and False Paper.cpp
@@ -0,0 +1,16 @@
+// C++ - True and False Paper
+// Rating - 398
+// https://www.codechef.com/submit/TFPAPER
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, k, n;
+	cin >> t;
+	while(t--) {
+	    cin >> k >> n;
+	    cout << k - n << endl;
+	}
+	return 0;
+}
