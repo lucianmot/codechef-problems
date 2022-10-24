2675024248edc5099f9648f996a126816c20babc
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-24 23:34:32 +0100

    solved problem

---
+++ 390 - C++ - Minimum Coins.cpp
@@ -0,0 +1,16 @@
+// C++ - Minimum Coins
+// Rating - 390
+// https://www.codechef.com/submit/MINCOINSREQ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    cout << x % 10 << endl;
+	}
+	return 0;
+}
