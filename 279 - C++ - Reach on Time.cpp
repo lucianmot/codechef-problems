f5d18f7d5ea6826ae9d3227df4a36ee879b08a11
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-25 11:30:19 +0100

    solved problem

---
+++ 279 - C++ - Reach on Time.cpp
@@ -0,0 +1,16 @@
+// C++ - Reach on Time
+// Rating - 279
+// https://www.codechef.com/submit/TIMELY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    x >= 30 ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
