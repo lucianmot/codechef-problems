4bbf357c76ee699d59bf14d6e34e8669da818a06
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-25 12:24:22 +0100

    problem solved

---
+++ 401 - C++ - Discount.cpp
@@ -0,0 +1,16 @@
+// C++ - Discount
+// Rating - 401
+// https://www.codechef.com/submit/DISCNT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    cout << 100 - x << endl;
+	}
+	return 0;
+}
