b03ec58a26e3d56d46f6a8be7652938a2ecbe6f6
Lucian Mot <lucian.i.mot@gmail.com>
2022-10-26 21:28:28 +0100

    solved problem

---
+++ 405 - C++ - Heigh of Rationals.cpp
@@ -0,0 +1,13 @@
+// C++ - Height of Rationals
+// Rating - 405
+// https://www.codechef.com/submit/HEIGHTRATION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	a > b ? cout << a << endl : cout << b << endl;
+	return 0;
+}
