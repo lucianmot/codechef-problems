b0601d9f3a9f487983255a439e8e7aa0787bf9ac
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-07 23:04:16 +0200

    solved problem

---
+++ 435 - C++ - Maximum Submissions.cpp
@@ -0,0 +1,16 @@
+// C++ - Maximum Submissions
+// Rating - 435
+// https://www.codechef.com/submit/MAXIMUMSUBS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, x;
+    cin >> t;
+    while (t--) {
+        cin >> x;
+        cout << x * 2 << endl;
+    }
+    return 0;
+}
