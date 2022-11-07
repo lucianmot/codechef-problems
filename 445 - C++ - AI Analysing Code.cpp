b10b7b019c15e0586948e8860cb2d37553805142
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-07 23:33:15 +0200

    solved problem

---
+++ 445 - C++ - AI Analysing Code.cpp
@@ -0,0 +1,13 @@
+// C++ - AI Analysing Code
+// Rating - 445
+// https://www.codechef.com/submit/AIANALYSE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    x <= 1000 ? cout << "Yes" << endl : cout << "No" << endl;
+    return 0;
+}
