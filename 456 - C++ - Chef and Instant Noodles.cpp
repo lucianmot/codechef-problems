e3eba9e9689592155cb96e96a22b36939ee4d725
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-08 23:29:27 +0200

    solved problem

---
+++ 456 - C++ - Chef and Instant Noodles.cpp
@@ -0,0 +1,13 @@
+// C++ - Chef and Instant Noodles
+// Rating - 456
+// https://www.codechef.com/submit/INSTNOODLE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << x * y << endl;
+    return 0;
+}
