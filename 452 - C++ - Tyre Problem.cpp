56f088ed36c1eecab117c32830f0abc650e54878
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-08 23:26:44 +0200

    problem solved

---
+++ 452 - C++ - Tyre Problem.cpp
@@ -0,0 +1,16 @@
+// C++ - Tyre Problem
+// Rating - 452
+// https://www.codechef.com/submit/TYRE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n, m;
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+        cout << n * 2 + m * 4 << endl;
+    }
+    return 0;
+}
