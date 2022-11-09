8c01c64f627a97031f3f2c97e878bc657f49b627
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-09 19:40:15 +0200

    solved problem in contest

---
+++ C++ - Water Consumption.cpp
@@ -0,0 +1,16 @@
+// C++ - Water Consumption
+// Rating - ---
+// https://www.codechef.com/START64D/problems/WATERCONS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, x;
+    cin >> t;
+    while (t--) {
+        cin >> x;
+        x >= 2000 ? cout << "YES" << endl : cout << "NO" << endl;
+    }
+    return 0;
+}
