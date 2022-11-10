892caa095f19db3a010a66d5be4110ca7aee3615
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-10 21:43:52 +0200

    update problem

--- C++ - Lucky Four.cpp
+++ C++ - Lucky Four.cpp
@@ -1,24 +1,24 @@
 // C++ - Lucky Four
 // Rating - ---
-// https://www.codechef.com/LP0TO101/problems/LUCKFOUR
+// https://www.codechef.com/LP1TO205/problems/LUCKFOUR
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int t;
-	cin >> t;
-	while(t--){
-	    int num;
-	    cin >> num;
-	    int answer = 0;
-	    for ( int i = num; i > 0; ) {
-	        if ( i % 10  == 4 ) {
-	            answer+=1;
-	        }
-	        i/=10;
-	    }
-	    cout << answer << endl;
-	}
-	return 0;
+    int t;
+    cin >> t;
+    while (t--) {
+        int num;
+        cin >> num;
+        int answer = 0;
+        for (int i = num; i > 0;) {
+            if (i % 10 == 4) {
+                answer += 1;
+            }
+            i /= 10;
+        }
+        cout << answer << endl;
+    }
+    return 0;
 }
