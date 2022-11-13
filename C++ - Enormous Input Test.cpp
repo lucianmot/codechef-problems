e09cbc859748c09eee38a8d661adeae7b7699cb5
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-13 22:53:37 +0200

    updated problem

--- C++ - Enormous Input Test.cpp
+++ C++ - Enormous Input Test.cpp
@@ -1,28 +1,26 @@
 // C ++ - Enormous Input Test
-// Rating - ---
-// https://www.codechef.com/LP0TO101/problems/INTEST
+// Rating - 464
+// https://www.codechef.com/problems/INTEST
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    
+
     int n, k;
-    
     cin >> n >> k;
-    
+
     int count = 0;
-    
-    for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        if ( num % k == 0) {
-            count+=1;
+    for (int i = 0; i <= n - 1; i++) {
+        int a;
+        cin >> a;
+        if (a % k == 0) {
+            count++;
         }
     }
+
     cout << count << endl;
     return 0;
 }
