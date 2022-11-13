5c864d4a788afd558466dd7741ee16e4269b2cc6
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-13 22:26:08 +0200

    solved problem

---
+++ 459 - C++ - Credit score.cpp
@@ -0,0 +1,13 @@
+// C++ - Credit score
+// Rating - 459
+// codechef.com/problems/CREDSCORE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    x >= 750 ? cout << "YES" << endl : cout << "NO" << endl;
+    return 0;
+}
