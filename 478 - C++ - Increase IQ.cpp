559a2c6175a49e9b31c6f41f9024a3cb9401b992
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-15 14:41:41 +0200

    solved problem

---
+++ 478 - C++ - Increase IQ.cpp
@@ -0,0 +1,13 @@
+// C++ - Increase IQ
+// Rating - 478
+// https://www.codechef.com/problems/INCRIQ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    x + 7 > 170 ? cout << "Yes" << endl : cout << "No" << endl;
+    return 0;
+}
