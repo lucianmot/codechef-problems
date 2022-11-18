b64e53d43aa64d16f1555451d976ec381e2f5a31
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-18 23:48:40 +0200

    finished solving  problem

--- 488 - C++ - Dominant Army.cpp
+++ 488 - C++ - Dominant Army.cpp
@@ -1,3 +1,20 @@
 // C++ - Dominant Army
 // Rating - 488
 // https://www.codechef.com/problems/DOMINANT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int armies[3];
+        for (int i = 0; i < 3; i++) {
+            cin >> armies[i];
+        }
+        sort(armies, armies + 3);
+        armies[0] + armies[1] < armies[2] ? cout << "YES" << endl : cout << "NO" << endl;
+    }
+    return 0;
+}
