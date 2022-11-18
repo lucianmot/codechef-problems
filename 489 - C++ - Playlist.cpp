91fbf5c5e2de477d0d88da48ddc4997ae0010533
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-18 23:53:30 +0200

    solved problem

--- 489 - C++ - Playlist.cpp
+++ 489 - C++ - Playlist.cpp
@@ -1,3 +1,16 @@
 // C++ - Playlist
 // Rating - 489
 // https://www.codechef.com/problems/SONGS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n, x;
+    cin >> t;
+    while (t--) {
+        cin >> n >> x;
+        cout << n / (3 * x) << endl;
+    }
+    return 0;
+}
