e225d881b524ac8b12f165e55e8d6d5677185568
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-19 22:26:15 +0200

    solved problem

--- 492 - C++ - Chef and Chocolates.cpp
+++ 492 - C++ - Chef and Chocolates.cpp
@@ -1,3 +1,22 @@
 // C++ - Chef and Chocolates
 // Rating - 492
 // https://www.codechef.com/problems/CHEFCHOCO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, c, x, y;
+    cin >> t;
+    while (t--) {
+        cin >> c >> x >> y;
+        int sum = x;
+        int answer = 0;
+        while (c >= sum + 1) {
+            sum += 1;
+            answer += y;
+        }
+        cout << answer << endl;
+    }
+    return 0;
+}
