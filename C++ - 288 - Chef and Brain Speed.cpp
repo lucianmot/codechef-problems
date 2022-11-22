78facd865eb19fe4b4ce1fb146ade840fec3ae84
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-22 10:22:15 +0200

    solve problem

---
+++ C++ - 288 - Chef and Brain Speed.cpp
@@ -0,0 +1,13 @@
+// C++ - Chef and Brain Speed
+// Rating - 288
+// https://www.codechef.com/problems/CBSPEED
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    x < y ? cout << "YES" << endl : cout << "NO" << endl;
+    return 0;
+}
