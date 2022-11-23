5c2451d4514db63c01ef154e74252f7f470a777e
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-23 20:09:47 +0200

    solve problem in contests

---
+++ C++ - Squats.cpp
@@ -0,0 +1,16 @@
+// C++ - Squats
+// Rating - ---
+// https://www.codechef.com/START66D/problems/SQUATS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, x;
+    cin >> t;
+    while (t--) {
+        cin >> x;
+        cout << 15 * x << endl;
+    }
+    return 0;
+}
