e85960ee9c9a2834b7a5130f08fb588c5db1bcd7
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-26 22:47:21 +0200

    solved problem

--- 527 - C++ - Janmash and Coins.cpp
+++ 527 - C++ - Janmash and Coins.cpp
@@ -2,3 +2,15 @@
 // Rating - 527
 // https://www.codechef.com/problems/JCOINS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << x * 10 + (y * 5) << endl;
+	}
+	return 0;
+}
