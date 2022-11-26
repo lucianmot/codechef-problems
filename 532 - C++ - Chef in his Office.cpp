282b11a7eddd423a3ff6d9845f9e35bf670d6299
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-26 22:49:34 +0200

    solved problem

--- 532 - C++ - Chef in his Office.cpp
+++ 532 - C++ - Chef in his Office.cpp
@@ -2,3 +2,15 @@
 // Rating - 532
 // https://www.codechef.com/problems/OFFICE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << x * 4 + y << endl;
+	}
+	return 0;
+}
