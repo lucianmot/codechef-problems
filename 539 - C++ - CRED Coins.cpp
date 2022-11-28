b8e046e309f0ba763c7199973dc672486e934e6f
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-28 23:47:13 +0200

    solved problem

--- 539 - C++ - CRED Coins.cpp
+++ 539 - C++ - CRED Coins.cpp
@@ -2,3 +2,15 @@
 // Rating - 539
 // https://www.codechef.com/problems/CREDCOINS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << (x * y) / 100 << endl;
+	}
+	return 0;
+}
