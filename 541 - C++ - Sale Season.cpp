2c19ac074b8eec7534e856d11e04a34a417a7872
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-29 01:32:57 +0200

    solved problem

--- 541 - C++ - Sale Season.cpp
+++ 541 - C++ - Sale Season.cpp
@@ -2,3 +2,23 @@
 // Rating - 541
 // https://www.codechef.com/problems/SALESEASON
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if (x <= 100) {
+	        cout << x << endl;
+	    } else if (x <= 1000) {
+	        cout << x - 25 << endl;
+	    } else if (x <= 5000) {
+	        cout << x - 100 << endl;
+	    } else if (x > 5000) {
+	        cout << x - 500 << endl;
+	    };
+	}
+	return 0;
+}
