c54a7b44995df5388ed2a8dd42cbd1811d6f18cf
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-29 21:33:07 +0200

    solved problem

--- 550 - C++ - Chefland Games.cpp
+++ 550 - C++ - Chefland Games.cpp
@@ -2,3 +2,15 @@
 // Rating - 550
 // https://www.codechef.com/problems/CHEFGAMES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, r1, r2, r3, r4;
+	cin >> t;
+	while(t--) {
+	    cin >> r1 >> r2 >> r3 >> r4;
+	    r1 == 0 && r2 == 0 && r3 == 0 && r4 == 0 ? cout << "IN" << endl : cout << "OUT" << endl; 
+	}
+	return 0;
+}
