f25293f99db25a4b459aac52e4c21664a96d1881
Lucian Mot <lucian.i.mot@gmail.com>
2022-11-30 21:49:24 +0200

    solved problem

--- 558 - C++ - Test Averages.cpp
+++ 558 - C++ - Test Averages.cpp
@@ -2,3 +2,15 @@
 // Rating - 558
 // https://www.codechef.com/problems/TESTAVG
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    (a+b)/2 < 35 || (a+c)/2 < 35 || (b+c)/2 < 35 ? cout << "fail" << endl : cout << "pass" << endl;
+	}
+	return 0;
+}
