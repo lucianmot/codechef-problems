273fc10b55eec71b7eb428c07046ccebbea15cd3
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-01 22:35:50 +0200

    solved problem

--- 562 - C++ -  Chef and NextGen.cpp
+++ 562 - C++ -  Chef and NextGen.cpp
@@ -1,3 +1,16 @@
 // C++ - Chef and NextGen
 // Rating - 562
 // https://www.codechef.com/problems/HELIUM3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, a, b, x, y;
+    cin >> t;
+    while(t--) {
+        cin >> a >> b >> x >> y;
+        a * b <= x * y ? cout << "Yes" << endl : cout << "No" << endl;
+    }
+	return 0;
+}
