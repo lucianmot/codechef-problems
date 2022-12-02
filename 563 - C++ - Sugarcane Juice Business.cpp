e60797c7bbe784c254f81c61e5a9c47d7bff387c
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-02 21:21:46 +0200

    solved problem

--- 563 - C++ - Sugarcane Juice Business.cpp
+++ 563 - C++ - Sugarcane Juice Business.cpp
@@ -2,3 +2,17 @@
 // Rating - 563
 // https://www.codechef.com/problems/SUGARCANE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    long aSum = 50 * n;
+	    int answer = aSum * 0.3;
+	    cout << answer << endl;
+	}
+	return 0;
+}
