e0764831504cb6b2703664492e932a3a27546363
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-03 15:47:48 +0200

    solved problem

--- 563 - C++ - Count the Notebooks.cpp
+++ 563 - C++ - Count the Notebooks.cpp
@@ -2,3 +2,15 @@
 // Rating - 563
 // https://www.codechef.com/problems/NOTEBOOK
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    cout << n * 10 << endl;
+	}
+	return 0;
+}
