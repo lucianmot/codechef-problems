58c778d92af39d1f161ed2248eb13ab60b98f0f6
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-03 23:44:02 +0200

    solved problem

--- 570 - C++ - Chef and Candies.cpp
+++ 570 - C++ - Chef and Candies.cpp
@@ -1,3 +1,23 @@
 // C++ - Chef and Candies
 // Rating - 570
 // https://www.codechef.com/problems/CHEFCAND
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    int diff = n - x;
+	    if (diff <= 0) {
+	        cout << 0 << endl;
+	    } else {
+	        int answer;
+	        diff % 4 == 0 ? answer = diff / 4 : answer = (diff / 4) + 1;
+	        cout << answer << endl;
+	    }
+	}
+	return 0;
+}
