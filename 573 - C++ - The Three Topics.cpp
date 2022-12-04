eaa5156a4ba4cf9a6ffa7c8e2f1c5feeb960d509
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-04 22:57:25 +0200

    solved problem

--- 573 - C++ - The Three Topics.cpp
+++ 573 - C++ - The Three Topics.cpp
@@ -1,3 +1,13 @@
 // C++ - The Three Topics
 // Rating - 573
 // https://www.codechef.com/problems/THREETOPICS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b, c, x;
+	cin >> a >> b >> c >> x;
+	a == x || b == x || c == x ? cout << "Yes" << endl : cout << "No" << endl;
+	return 0;
+}
