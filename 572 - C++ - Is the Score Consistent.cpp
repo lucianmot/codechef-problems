8751fae87536a1ee01443af4eeaa23b3569e236b
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-04 22:51:19 +0200

    solved problem

--- 572 - C++ - Is the Score Consistent.cpp
+++ 572 - C++ - Is the Score Consistent.cpp
@@ -2,3 +2,15 @@
 // Rating - 572
 // https://www.codechef.com/problems/TRUESCORE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, d;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c >> d;
+	    a <= c && b <= d ? cout << "POSSIBLE" << endl : cout << "IMPOSSIBLE" << endl;
+	}
+	return 0;
+}
