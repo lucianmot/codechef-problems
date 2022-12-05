62c0316d574a33f4a6150a74284d1e915091d7bc
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-05 12:51:55 +0200

    solved

--- 580 - C++ - Problems in your to-do list.cpp
+++ 580 - C++ - Problems in your to-do list.cpp
@@ -2,3 +2,21 @@
 // Rating - 580
 // https://www.codechef.com/problems/TODOLIST
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int theCounter = 0;
+	    for (int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        if (num >= 1000) theCounter++;
+	    }
+	    cout << theCounter << endl;
+	}
+	return 0;
+}
