6d705f58f74ef7c13a5174f6cddeb9475e9f4bab
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-09 14:25:39 +0000

    solved simple problem

---
+++ 283 - C++ - Tour of King.cpp
@@ -0,0 +1,16 @@
+// C++ - Tour of King
+// Rating - 283
+// https://www.codechef.com/problems/KNGTOR
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    cout << n * 5 + m * 7 << endl;
+	}
+	return 0;
+}
