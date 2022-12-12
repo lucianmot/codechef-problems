814709850a9492094d0c3d56e5e84fb45f599ccf
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-12 09:43:13 +0000

    solved a tiny problem

---
+++ C++ - Integers that sum to the same value.cpp
@@ -0,0 +1,13 @@
+// C++ - Integers that sum to the same value
+// Rating - 592
+// https://www.codechef.com/problems/SUMNEQ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << n-1 << endl;
+	return 0;
+}
