1aa25dde07435a5c92ac46b9f3864acd638d132f
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-15 17:43:46 +0000

    added solution

--- 608 - C++ - Minimum Cars required.cpp
+++ 608 - C++ - Minimum Cars required.cpp
@@ -2,3 +2,17 @@
 // Rating - 608
 // https://www.codechef.com/problems/MINCARS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer;
+	    n % 4 == 0 ? answer = n / 4 : answer = (n / 4) + 1;
+	    cout << answer << endl;
+	}
+	return 0;
+}
