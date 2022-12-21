68121528329f9fd7ff6222c1adbfd8dc45561986
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-21 21:16:01 +0000

    added solution

--- 622 - C++ - Discus Throw.cpp
+++ 622 - C++ - Discus Throw.cpp
@@ -2,3 +2,17 @@
 // Rating - 622
 // https://www.codechef.com/problems/DISCUS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    int bigList[3] = {a, b, c};
+	    sort(bigList, bigList + 3);
+	    cout << bigList[2] << endl;
+	}
+	return 0;
+}
