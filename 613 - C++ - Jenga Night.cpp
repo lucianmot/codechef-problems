2affff2229295979c4b7e39046f90b02b18c8fc9
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-21 15:05:15 +0000

    solved problem

--- 613 - C++ - Jenga Night.cpp
+++ 613 - C++ - Jenga Night.cpp
@@ -2,3 +2,15 @@
 // Rating - 613
 // https://www.codechef.com/problems/JENGA
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    n <= x ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
