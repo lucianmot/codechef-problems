cb81056e765eb8e167e61b349e1eeb78c111cec3
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-21 15:15:08 +0000

    fixed previous solution

--- 613 - C++ - Jenga Night.cpp
+++ 613 - C++ - Jenga Night.cpp
@@ -5,12 +5,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#include <bits/stdc++.h>
+using namespace std;
+
 int main() {
 	int t, n, x;
 	cin >> t;
 	while(t--) {
 	    cin >> n >> x;
-	    n <= x ? cout << "YES" << endl : cout << "NO" << endl;
+	    n <= x && x % n == 0 ? cout << "YES" << endl : cout << "NO" << endl;
 	}
 	return 0;
 }
