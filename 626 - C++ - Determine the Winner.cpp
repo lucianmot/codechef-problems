3f175930b8509a13b6ba9cd0f344f1db1b74061f
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-22 21:28:32 +0000

    solved problem

--- 626 - C++ - Determine the Winner.cpp
+++ 626 - C++ - Determine the Winner.cpp
@@ -2,3 +2,23 @@
 // Rating - 626
 // https://www.codechef.com/problems/WINNERR
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> x >> y;
+	    a > b ? c = a : c = b;
+	    x > y ? z = x : z = y;
+	    if ( c == z ) {
+	        cout << "TIE" << endl;
+	    } else if ( c > z ) {
+	        cout << "Q" << endl;
+	    } else if ( c < z ) {
+	        cout << "P" << endl;
+	    }
+	}
+	return 0;
+}
