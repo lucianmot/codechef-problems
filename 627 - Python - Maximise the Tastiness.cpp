04768237b66ea22ddc5216f440ff48a706e312e2
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-23 17:33:24 +0000

    solved problem

--- 627 - Python - Maximise the Tastiness.cpp
+++ 627 - Python - Maximise the Tastiness.cpp
@@ -2,3 +2,17 @@
 // Rating - 627
 // https://www.codechef.com/problems/MAXTASTE
 
+testCases = int(input())
+
+for _ in range(0, testCases):
+    a, b, c, d = map(int, input().split())
+    if ( a > b ):
+        x = a
+    else:
+        x = b
+    if ( c > d ):
+        y = c
+    else:
+        y = d
+    
+    print(x+y)
