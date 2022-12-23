98eb821240ee4f8684df4bf25cdb3a6b8417cb0e
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-23 17:41:57 +0000

    solved problem

--- 627 - C++ - Maximise the Tastiness.cpp
+++ 627 - C++ - Maximise the Tastiness.cpp
@@ -2,3 +2,18 @@
 // Rating - 627
 // https://www.codechef.com/problems/MAXTASTE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, d;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c >> d;
+	    int x, y;
+	    a > b ? x = a : x = b;
+	    c > d ? y = c : y = d;
+	    cout << x + y << endl;
+	}
+	return 0;
+}
