f730bd73f35d708d30529349661217849ba56069
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-24 18:53:36 +0000

    solved problem

--- 628 - C++ - Watching Movies at 2x.cpp
+++ 628 - C++ - Watching Movies at 2x.cpp
@@ -2,3 +2,12 @@
 // Rating - 628
 // https://www.codechef.com/problems/MOVIE2X
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	cout << x - (y/2) << endl;
+	return 0;
+}
