d8759ffb0bc1aa7d382121cbf12d05f750114147
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-25 23:24:14 +0000

    solved problem

--- 639 - C++ - Police and Thief.cpp
+++ 639 - C++ - Police and Thief.cpp
@@ -2,3 +2,21 @@
 // Rating - 639
 // https://www.codechef.com/problems/POLTHIEF
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    if ( x == y ) {
+	        cout << 0 << endl;
+	    } else if (x > y) {
+	        cout << x - y << endl;
+	    } else if (x < y) {
+	        cout << y - x << endl;
+	    }
+	}
+	return 0;
+}
