ab02e2efc976ef8d556f151deb3dc052773c2726
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-26 20:35:24 +0000

    added solution

--- 646 - C++ - Finding Shoes.cpp
+++ 646 - C++ - Finding Shoes.cpp
@@ -2,3 +2,16 @@
 // Rating - 646
 // https://www.codechef.com/problems/FINDSHOES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    int p = n > m ? n - m : 0;
+	    cout << n + p << endl;
+	}
+	return 0;
+}
