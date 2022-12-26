67ab2c99a8442e9d053d460350f1e3d9ec35643b
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-26 19:23:04 +0000

    solved problem

--- 643 - C++ - Bath in Winters.cpp
+++ 643 - C++ - Bath in Winters.cpp
@@ -2,3 +2,15 @@
 // Rating - 643
 // https://www.codechef.com/problems/BATH
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << x / (2 * y) << endl;
+	}
+	return 0;
+}
