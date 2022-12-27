28c36eacc45687dfb106dbc20309535043119c8a
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-27 23:03:31 +0000

    solved problem

--- 651 - C++ - Chess Ratings.cpp
+++ 651 - C++ - Chess Ratings.cpp
@@ -2,3 +2,20 @@
 // Rating - 651
 // https://www.codechef.com/problems/C_RATING
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    if ( x == y ) {
+	        cout << "0" << endl;
+	    } else {
+	        int answer = ceil((y - x) / 8.0);
+	        cout << answer << endl;
+	    }
+	}
+	return 0;
+}
