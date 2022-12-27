78b09086f8267a3de103bd1f6173b57dd00c2a62
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-27 14:15:06 +0000

    solved problem

--- 649 - C++ - Mario and Transformation.cpp
+++ 649 - C++ - Mario and Transformation.cpp
@@ -2,3 +2,22 @@
 // Rating - 649
 // https://www.codechef.com/problems/TRANSFORM
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = n % 3;
+	    if ( answer == 0 ) {
+	        cout << "NORMAL" << endl;
+	    } else if ( answer == 1) {
+	        cout << "HUGE" << endl;
+	    } else if ( answer == 2 ) {
+	        cout << "SMALL" << endl;
+	    }
+	}
+	return 0;
+}
