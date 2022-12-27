6e0384d324bffe2cb5b380c4c2c29bee7870a1f2
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-27 22:21:32 +0000

    solved problem

--- 650 - C++ - Mario and Bullet.cpp
+++ 650 - C++ - Mario and Bullet.cpp
@@ -2,3 +2,16 @@
 // Rating - 650
 // https://www.codechef.com/problems/BULLET
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int diff = y / x;
+	    diff >= z ? cout << "0" << endl : cout << z - diff << endl;
+	}
+	return 0;
+}
