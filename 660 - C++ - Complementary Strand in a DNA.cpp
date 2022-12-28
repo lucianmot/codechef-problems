59b329fe2a3edeaeeb87720f7971d1b80f8fbcc0
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-28 12:17:37 +0000

    added solution

--- 660 - C++ - Complementary Strand in a DNA.cpp
+++ 660 - C++ - Complementary Strand in a DNA.cpp
@@ -2,3 +2,28 @@
 // Rating - 660
 // https://www.codechef.com/problems/DNASTRAND
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    string word;
+	    cin >> word;
+	    for (int i = 0; i < n; i++) {
+	        if (word[i] == 'A') {
+	            word[i] = 'T';
+	        } else if (word[i] == 'T') {
+	            word[i] = 'A';
+	        } else if (word[i] == 'C') {
+	            word[i] = 'G';
+	        } else if (word[i] == 'G') {
+	            word[i] = 'C';
+	        }
+	    }
+	    cout << word << endl;
+	}
+	return 0;
+}
