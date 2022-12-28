90e167a859bbccf27bfffac908ae418f0a1cb80a
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-28 21:20:06 +0000

    solved problem

--- 455 - C++ - Perfect Trio.cpp
+++ 455 - C++ - Perfect Trio.cpp
@@ -2,3 +2,17 @@
 // Rating - 455
 // https://www.codechef.com/problems/PERFECTTRIO
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    int aList[3] = {a, b, c};
+	    sort(aList, aList + 3);
+	    aList[0] + aList[1] == aList[2] ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
