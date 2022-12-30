e31e6767f174e821f6d0ebd59f954921ab42d386
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-30 13:50:46 +0000

    solved problem

--- 660 - C++ - Change Row and Column Both.cpp
+++ 660 - C++ - Change Row and Column Both.cpp
@@ -2,3 +2,15 @@
 // Rating - 660
 // https://www.codechef.com/problems/CHANGEPOS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, d;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c >> d;
+	    a != c && b != d ? cout << 1 << endl : cout << 2 << endl;
+	}
+	return 0;
+}
