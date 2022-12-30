720ef48cb334f5038441e9bf407ff148a1176096
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-30 14:21:19 +0000

    solved problem

--- 662 - C++ - Chef and Water Battles.cpp
+++ 662 - C++ - Chef and Water Battles.cpp
@@ -2,3 +2,17 @@
 // Rating - 662
 // https://www.codechef.com/problems/CHEFBOTTLE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x, k;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x >> k;
+	    int filledBottles = floor(k / x);
+	    int answer = n < filledBottles ? n : filledBottles;
+	    cout << answer << endl;
+	}
+	return 0;
+}
