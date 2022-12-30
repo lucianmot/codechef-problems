81d0a16ba028434b23fc84ba1cf4b371f47bdfa8
Lucian Mot <lucian.i.mot@gmail.com>
2022-12-30 18:53:20 +0000

    solved problem

--- 668 - C++ - Candy Distribution.cpp
+++ 668 - C++ - Candy Distribution.cpp
@@ -2,3 +2,20 @@
 // Rating - 668
 // https://www.codechef.com/problems/CANDYDIST
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    int answer = floor(n/m);
+	    if (answer % 2 == 0 && answer * m == n) {
+	        cout << "Yes" << endl;
+	    } else {
+	        cout << "No" << endl;
+	    }
+	}
+	return 0;
+}
