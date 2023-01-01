78c2107c61c7f8a09356627a1a5dcdd66b7d8aa6
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-01 15:32:02 +0000

    solved problem

--- 679 - C++ - The Last Levels.cpp
+++ 679 - C++ - The Last Levels.cpp
@@ -2,3 +2,18 @@
 // Rating - 679
 // https://www.codechef.com/problems/LASTLEVELS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int calculationX = floor(x / 3.0);
+	    int videoGamesBreak = x % 3 == 0 ? calculationX - 1 : calculationX;
+	    int answer = x * y + videoGamesBreak * z;
+	    cout << answer << endl;
+	}
+	return 0;
+}
