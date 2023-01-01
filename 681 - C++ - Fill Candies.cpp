0fa61083d842541571aa06f017a9a86bcf5040a7
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-01 00:02:59 +0000

    solved problem

--- 681 - C++ - Fill Candies.cpp
+++ 681 - C++ - Fill Candies.cpp
@@ -1,3 +1,18 @@
 // C++ - Fill Candies
 // Rating - 681
 // https://www.codechef.com/problems/FILLCANDIES
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	double n, k, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> k >> m;
+	    int answer = ceil(n / (k * m));
+	    cout << answer << endl;
+	}
+	return 0;
+}
