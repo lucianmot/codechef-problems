41fdeec67331eef7fe829ec10446f258d92d5940
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-02 15:00:56 +0000

    solved problem

---
+++ 273 - C++ - Kitchen Timings.cpp
@@ -0,0 +1,16 @@
+// C++ - Kitchen Timings
+// Rating - 273
+// https://www.codechef.com/problems/KITCHENTIME
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << y - x << endl;
+	}
+	return 0;
+}
