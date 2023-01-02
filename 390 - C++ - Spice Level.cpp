cc276b6f24af9203899e9fbfb5b4341f94977985
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-02 15:31:09 +0000

    added solution

--- 390 - C++ - Spice Level.cpp
+++ 390 - C++ - Spice Level.cpp
@@ -2,3 +2,21 @@
 // Rating - 390
 // https://www.codechef.com/problems/KITCHENSPICE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if ( x < 4 ) {
+	        cout << "MILD" << endl;
+	    } else if ( x < 7 ) {
+	        cout << "MEDIUM" << endl;
+	    } else if ( x >= 7 ) {
+	        cout << "HOT" << endl;
+	    } 
+	}
+	return 0;
+}
