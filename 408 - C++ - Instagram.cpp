f922653bf6eded004d9316bd11e6aadab45c45f4
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-02 15:40:50 +0000

    added solution

--- 408 - C++ - Instagram.cpp
+++ 408 - C++ - Instagram.cpp
@@ -2,3 +2,16 @@
 // Rating - 408
 // https://www.codechef.com/problems/INSTAGRAM
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	double x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    x / y > 10.0 ? cout << "YES" << endl : cout << "NO" << endl;
+	} 
+	return 0;
+}
