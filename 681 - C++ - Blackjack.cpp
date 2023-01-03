4bb94a04d4834d9e3fe02c845223a359fbcda7b5
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-03 20:06:00 +0000

    solved added solution

--- 681 - C++ - Blackjack.cpp
+++ 681 - C++ - Blackjack.cpp
@@ -2,3 +2,16 @@
 // Rating - 681
 // https://www.codechef.com/problems/BLACKJACK
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b;
+	    int c = 21 - (a + b);
+	    c > 0 && c < 11 ? cout << c << endl : cout << -1 << endl;
+	}
+	return 0;
+}
