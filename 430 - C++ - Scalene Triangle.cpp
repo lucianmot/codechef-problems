b7cdbf669082a600a669fe17ccdcb25c16513f8c
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-03 00:32:09 +0000

    added solution

--- 430 - C++ - Scalene Triangle.cpp
+++ 430 - C++ - Scalene Triangle.cpp
@@ -2,3 +2,15 @@
 // Rating - 430
 // https://www.codechef.com/problems/SCALENE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    a != b && a != c && b != c ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
