b359ed8d323ef91a24b5b80648304ecb662618e4
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-05 19:59:56 +0000

    solvedp roblem

--- 686 - C++ - X Jumps.cpp
+++ 686 - C++ - X Jumps.cpp
@@ -2,3 +2,20 @@
 // Rating - 686
 // https://www.codechef.com/problems/XJUMP
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int times = 0;
+	    if ( y <= x && y > 1 ) {
+	        times = x / y;
+	    }
+	    int answer =  times + (x - times * y) * 1;
+	    cout << answer << endl;
+	}
+	return 0;
+}
