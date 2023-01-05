02f3b1ace2fcef80c99bf1af2ef6e6abcf2c39d3
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-05 20:44:24 +0000

    added solution

--- 308 - C++ - Sum it.cpp
+++ 308 - C++ - Sum it.cpp
@@ -2,3 +2,15 @@
 // Rating - 308
 // https://www.codechef.com/problems/SUMM
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    a + b == c ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
