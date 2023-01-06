3d1902f259fb351c1a8e1595002a56a980bb0e06
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-06 22:55:04 +0000

    solved problem

--- 691 - C++ - Valentine is Coming.cpp
+++ 691 - C++ - Valentine is Coming.cpp
@@ -2,3 +2,14 @@
 // Rating - 681
 // https://www.codechef.com/problems/VALENTINE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << x / y << endl;
+	}
+	return 0;
+}
