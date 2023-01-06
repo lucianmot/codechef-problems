5cd265939b366f5e0f4cb6493ba20f83fd1b273a
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-06 23:01:52 +0000

    added solution

--- 691 - C++ - It is My Serve.cpp
+++ 691 - C++ - It is My Serve.cpp
@@ -2,3 +2,16 @@
 // Rating - 691
 // https://www.codechef.com/problems/MYSERVE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, p, q;
+	cin >> t;
+	while(t--) {
+	    cin >> p >> q;
+	    int sumPoints = (p + q) / 2;
+	    sumPoints % 2 == 0 ? cout << "Alice" << endl : cout << "Bob" << endl;
+	}
+	return 0;
+}
