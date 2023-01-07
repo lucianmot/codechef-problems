dec487fba983901cbdad47f12e38bc26f2f0af22
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-07 15:56:08 +0000

    added solution

--- 697 - C++ - Weights.cpp
+++ 697 - C++ - Weights.cpp
@@ -2,3 +2,23 @@
 // Rating - 697
 // https://www.codechef.com/problems/WGHTS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, w, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> w >> x >> y >> z;
+	    if ( w == (x + y + z)) {
+	        cout << "YES" << endl;
+	    } else if ( w == (x + y) || w == (x + z) || w == (y + z) ) {
+	        cout << "YES" << endl;
+	    } else if ( w == x || w == y || w == z ) {
+	        cout << "YES" << endl;
+	    } else {
+	        cout << "NO" << endl;
+	    }
+	}
+	return 0;
+}
