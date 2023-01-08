bc937027a614e4f008f1caa488e443044a0d1ac6
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-08 22:49:51 +0000

    added solution

--- 702 - C++ - Chef and his Apps.cpp
+++ 702 - C++ - Chef and his Apps.cpp
@@ -2,3 +2,22 @@
 // Rating - 702
 // https://www.codechef.com/problems/CHEFAPPS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, s, x, y, z;
+    cin >> t;
+    while(t--) {
+        cin >> s >> x >> y >> z;
+        int remainingStorage = s - (x +y);
+        if ( z <= remainingStorage ) {
+            cout << 0 << endl;
+        } else if ( z > remainingStorage && (z <= (x + remainingStorage) || z <= (y + remainingStorage))) {
+            cout << 1 << endl;
+        } else {
+            cout << 2 << endl;
+        }
+    }
+	return 0;
+}
