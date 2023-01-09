fed7b67e6880bd10944fe7bbd56eb885838540f9
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-09 22:39:05 +0000

    added solution

--- 668 - C++ - Finding Square Roots.cpp
+++ 668 - C++ - Finding Square Roots.cpp
@@ -2,3 +2,15 @@
 // Rating - 668
 // https://www.codechef.com/problems/FSQRT
 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t, n;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << int(sqrt(n)) << endl;
+    }
+    return 0;
+}
