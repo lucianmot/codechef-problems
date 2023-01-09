15dcc023f937bfaad90a23b86ebd5c3c710de5c7
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-09 22:43:07 +0000

    added solution

--- 716 - C++ - Cup Finals.cpp
+++ 716 - C++ - Cup Finals.cpp
@@ -2,3 +2,15 @@
 // Rating - 716
 // https://www.codechef.com/problems/CRICUP
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, d;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> d;
+	    abs(x - y) <= d ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
