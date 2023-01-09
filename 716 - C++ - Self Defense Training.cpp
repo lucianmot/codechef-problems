10d074ffb2d9d7e64a86d2ce1e911a1c00e684fe
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-09 13:13:54 +0000

    added solution

--- 716 - C++ - Self Defense Training.cpp
+++ 716 - C++ - Self Defense Training.cpp
@@ -2,3 +2,23 @@
 // Rating - 716
 // https://www.codechef.com/problems/SELFDEF
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = 0;
+	    for (int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        if (num > 9 && num < 61) {
+	            answer++;
+	        }
+	    }
+	    cout << answer << endl;
+	}
+	return 0;
+}
