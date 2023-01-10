1452063cee9e3b48a1b39683a948478ccba44ced
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-10 23:08:22 +0000

    added solution

--- 421 - C++ - Find Remainder.cpp
+++ 421 - C++ - Find Remainder.cpp
@@ -2,3 +2,15 @@
 // Rating - 421
 // https://www.codechef.com/problems/FLOW002
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, a, b;
+    cin >> t;
+    while(t--) {
+        cin >> a >> b;
+        cout << a % b << endl;
+    }
+    return 0;
+}
