e87bb6884c0b58efebc66f18c1e7fd5e33a89add
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-10 23:05:06 +0000

    added solution

--- 718 - Speed Limit Test.cpp
+++ 718 - Speed Limit Test.cpp
@@ -2,3 +2,24 @@
 // Rating - 718
 // https://www.codechef.com/problems/SPEEDTEST
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	float a, x, b, y;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> x >> b >> y;
+	    float alice = a / x;
+	    float bob = b / y;
+	    if ( alice == bob ) {
+	        cout << "Equal" << endl;
+	    } else if ( alice > bob ) {
+	        cout << "Alice" << endl;
+	    } else if ( alice < bob ) {
+	        cout << "Bob" << endl;
+	    }
+	}
+	return 0;
+}
