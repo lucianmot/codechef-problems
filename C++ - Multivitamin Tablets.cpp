0bb70bb4ebe89710637d219217da6f602983d205
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-11 16:40:33 +0000

    added solution contest

--- C++ - Multivitamin Tablets.cpp
+++ C++ - Multivitamin Tablets.cpp
@@ -2,3 +2,15 @@
 // Rating - ---
 // https://www.codechef.com/START73D/problems/TABLETS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    x * 3 <= y ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
