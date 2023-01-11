3f1c2d7ac0802d030f7e519f56d5969dd240a970
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-11 16:38:39 +0000

    added solution

--- C++ - Chef Plays Ludo.cpp
+++ C++ - Chef Plays Ludo.cpp
@@ -2,3 +2,15 @@
 // Rating - ---
 // https://www.codechef.com/START73D/problems/LUDO
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    x == 6 ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
