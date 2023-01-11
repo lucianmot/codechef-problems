75d18809d29595a7d7d2bc33b5744979e9663e06
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-11 16:42:10 +0000

    added solution contest

--- C++ - Lucky Numbers.cpp
+++ C++ - Lucky Numbers.cpp
@@ -2,3 +2,24 @@
 // Rating - ---
 // https://www.codechef.com/START73D/problems/LUCKYN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+ 	    cin >> x;
+	    int answer = 0;
+	    while ( x > 6 ) {
+	        int lastDigit = x % 10;
+	        if (lastDigit == 7) {
+	            answer = 1;
+	            break;
+	        }
+	        x/=10;
+	    }
+	    answer == 1 ? cout << "YES" << endl : cout << "NO" << endl;
+	}
+	return 0;
+}
