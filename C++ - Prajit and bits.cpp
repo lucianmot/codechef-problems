7579db04aa64ba022d4be7cd962cf848a6f82761
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-12 09:04:34 +0000

    added solution

--- C++ - Prajit and bits.cpp
+++ C++ - Prajit and bits.cpp
@@ -2,3 +2,24 @@
 // Rating - ---
 // https://www.codechef.com/LP2TO305/problems/ADDI
 
+#define ll long long
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	ll n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int count = 0;
+	    while(n > 0) {
+	        if ((n&1) == 0) {
+	            count++;
+	        }
+	        n = n >> 1;
+	    }
+	    cout << count << endl;
+	}
+	return 0;
+}
