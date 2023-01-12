7823137136b1cb3f94930107f8a17a524d8176e5
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-12 09:01:49 +0000

    added solution

--- 711 - C++ - Minimum number of coins.cpp
+++ 711 - C++ - Minimum number of coins.cpp
@@ -2,3 +2,20 @@
 // Rating - 711
 // https://www.codechef.com/problems/MINCOINS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if ( x % 5 == 0 ) {
+	        int c = round(float(x) / 10);
+	        cout << c << endl;
+	    } else {
+	        cout << -1 << endl;
+	    }
+	}
+	return 0;
+}
