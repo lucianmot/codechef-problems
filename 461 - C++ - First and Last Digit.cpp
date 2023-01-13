b28c78ce8f584259635563dbcec32bfd20ec8197
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-13 22:25:07 +0000

    added solution

--- 461 - C++ - First and Last Digit.cpp
+++ 461 - C++ - First and Last Digit.cpp
@@ -2,3 +2,20 @@
 // Rating - 461
 // https://www.codechef.com/problems/FLOW004
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int t;
+   cin>>t;
+   while(t--) {
+       int n;
+       cin >> n;
+       int rem = n % 10;
+       while(n > 9) {
+           n = n/10;
+       }
+       cout << n+rem << "\n";
+   }
+return 0;
+}
