c8a59c4a28fdebcbc4281ef378c46f73f7e2231e
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-13 22:11:33 +0000

    added solution

--- 410 - C++ - ATM.cpp
+++ 410 - C++ - ATM.cpp
@@ -2,3 +2,23 @@
 // Rating - 410
 // https://www.codechef.com/problems/HS08TEST
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(2);
+
+    int a;
+    float b;
+    cin >> a >> b;
+    
+    if (( a % 5 == 0 ) && ( b - a -0.5 >= 0)) {
+        cout << fixed << b-a-0.5 << endl;
+    } else {
+        cout << fixed << b << endl;
+    }
+
+    return 0;
+}
