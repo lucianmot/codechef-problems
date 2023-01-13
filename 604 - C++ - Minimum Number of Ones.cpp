1719f9475717e923c7b7b7445a960cb2f40db78d
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-13 22:20:10 +0000

    added solution

--- 604 - C++ - Minimum Number of Ones.cpp
+++ 604 - C++ - Minimum Number of Ones.cpp
@@ -1,3 +1,22 @@
 // C++ - Minimum Number of Ones
 // Rating - 604
 // https://www.codechef.com/problems/MINNOO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = 0;
+	    for (int i = 0; i < n; i++) {
+	        if (i % 2 != 0) {
+	            answer++;
+	        }
+	    }
+	    cout << answer << endl;
+	}
+	return 0;
+}
