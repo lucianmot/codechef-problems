e4c819bb940f9af644af7e7dbd5e5958b4ec671a
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-14 20:33:01 +0000

    added solution

--- C++ - Overspeeding Fine.cpp
+++ C++ - Overspeeding Fine.cpp
@@ -2,3 +2,21 @@
 // Rating - ---
 // https://www.codechef.com/JAN231D/problems/FINE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if ( x <= 70 ) {
+	        cout << 0 << "\n";
+	    } else if ( x > 70 && x <= 100 ) {
+	        cout << 500 << "\n";
+	    } else if ( x > 100 ) {
+	        cout << 2000 << "\n";
+	    }
+	}
+	return 0;
+}
