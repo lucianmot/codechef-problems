01ec27ebe18e2bf0d4e72a28c0cc75b5beb52e79
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-14 20:58:07 +0000

    resolved a gcd lcm problem

--- 635 - C++ - GCD and LCM.cpp
+++ 635 - C++ - GCD and LCM.cpp
@@ -2,27 +2,19 @@
 // Rating - 635
 // https://www.codechef.com/problems/FLOW016
 
-// Unsolved remains to be completed
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-	int t, a, b;
+	int t;
+	long long a, b;
 	cin >> t;
 	while(t--) {
 	    cin >> a >> b;
-	    int gcd = min(a, b);
-	    while(gcd > 0) {
-	        if (a % gcd == 0 && b % gcd == 0) {
-	            break;
-	        }
-	        gcd--;
-	    }
-	    int lcm = (a * b) / gcd;
-	    cout << gcd << " " << lcm << endl;
+		long long theGcd = __gcd(a, b);
+ 	    cout << theGcd << " " << (a*b)/theGcd << "\n";
 	}
 	return 0;
 }
