b7729b8e23c77cf480ffe2f3d9149739b0ee5ae7
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-14 20:34:31 +0000

    added solution

--- C++ - Negative Product.cpp
+++ C++ - Negative Product.cpp
@@ -2,3 +2,19 @@
 // Rating - ---
 // https://www.codechef.com/JAN231D/problems/NEGPROD
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    if ( (a * b) < 0 || (a * c) < 0 || (b * c) < 0 ) {
+	        cout << "YES" << "\n";
+	    } else {
+	        cout << "NO" << "\n";
+	    }
+	}
+	return 0;
+}
