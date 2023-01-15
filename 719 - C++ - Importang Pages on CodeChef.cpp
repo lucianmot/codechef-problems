10979776a8f85bc86579ae4dde24c5c197a384f9
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-15 22:26:31 +0000

    added solution

--- 719 - C++ - Importang Pages on CodeChef.cpp
+++ 719 - C++ - Importang Pages on CodeChef.cpp
@@ -2,3 +2,18 @@
 // Rating - 719
 // https://www.codechef.com/problems/CHEFPAGES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if ( a == 1 && b == 1 ) {
+	    cout << "https://discuss.codechef.com" << "\n";
+	} else if ( a == 1 && b == 0 ) {
+	    cout << "https://www.codechef.com/contests" << "\n";
+	} else {
+	    cout << "https://www.codechef.com/practice" << "\n";
+	}
+	return 0;
+}
