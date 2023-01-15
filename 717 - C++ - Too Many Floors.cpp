29994774d00353a1742d40c52758917b5f951005
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-15 22:45:44 +0000

    added solution

--- 717 - C++ - Too Many Floors.cpp
+++ 717 - C++ - Too Many Floors.cpp
@@ -2,3 +2,18 @@
 // Rating - 717
 // https://www.codechef.com/problems/FLOORS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int xFloor = ((x-1)/10)+1;
+	    int yFloor = ((y-1)/10)+1;
+	    int answer = abs(xFloor-yFloor);
+	    cout << answer << "\n";
+	}
+	return 0;
+}
