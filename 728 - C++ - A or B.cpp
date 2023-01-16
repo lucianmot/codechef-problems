6cb9339ea00c1cb9f3c1d79405d2fbd2c8d7c296
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-16 22:48:07 +0000

    added solution

--- 728 - C++ - A or B.cpp
+++ 728 - C++ - A or B.cpp
@@ -2,3 +2,17 @@
 // Rating - 728
 // https://www.codechef.com/problems/AORB
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int answer = 1500 - x * 2 - (x + y) * 4;
+	    int answer2 = 1500 - (x + y) * 2 - y * 4;
+	    answer > answer2 ? cout << answer << "\n" : cout << answer2 << "\n";
+	}
+	return 0;
+}
