03252319635ba09e0b4bdc743a403d962c9300e6
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-16 22:41:21 +0000

    added solution

--- 722 - C++ - Decrement or Increment.cpp
+++ 722 - C++ - Decrement or Increment.cpp
@@ -2,3 +2,14 @@
 // Rating - 722
 // https://www.codechef.com/problems/DECINC
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int n;
+	cin >> n;
+	n % 4 == 0 ? cout << n+1 << "\n" : cout << n-1 << "\n";
+	return 0;
+}
