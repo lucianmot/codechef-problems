f0658a83fc3abb961ff4cec0916cd48386b641d5
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-16 22:36:47 +0000

    added solution

--- 730 - C++ - Second Largest.cpp
+++ 730 - C++ - Second Largest.cpp
@@ -2,3 +2,19 @@
 // Rating - 730
 // https://www.codechef.com/problems/FLOW017
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    int smallList[3] = {a, b, c};
+	    sort(smallList, smallList + 3);
+	    cout << smallList[1] << "\n";
+	}
+	return 0;
+}
