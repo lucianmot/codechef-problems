a68df74d27a840bc16c0d274dbad33f746f370f9
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-17 15:55:00 +0000

    added solution

--- 749 - C++ - Sum or Difference.cpp
+++ 749 - C++ - Sum or Difference.cpp
@@ -2,3 +2,16 @@
 // Rating - 749
 // https://www.codechef.com/problems/DIFFSUM
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int n1, n2;
+	cin >> n1 >> n2;
+	int elSum = n1 + n2;
+	int elDiff = n1 - n2;
+	n1 > n2 ? cout << elDiff << "\n" : cout << elSum << "\n";
+	return 0;
+}
