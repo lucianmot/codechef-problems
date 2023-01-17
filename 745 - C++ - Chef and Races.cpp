2ab42d466d2f236df7327efc0191baba10f78971
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-17 15:50:44 +0000

    added solution

--- 745 - C++ - Chef and Races.cpp
+++ 745 - C++ - Chef and Races.cpp
@@ -2,3 +2,20 @@
 // Rating - 745
 // https://www.codechef.com/problems/CHEFRACES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> a >> b;
+	    int answer = 0;
+	    if ( x != a && x != b )
+	        answer++;
+	    if ( y != a && y != b )
+	        answer++;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
