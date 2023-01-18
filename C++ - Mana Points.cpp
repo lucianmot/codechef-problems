7d2cd224ab83929816c830d68ea455638988e843
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-18 15:48:41 +0000

    added solution

--- C++ - Mana Points.cpp
+++ C++ - Mana Points.cpp
@@ -2,3 +2,15 @@
 // Rating - ---
 // https://www.codechef.com/START74D/problems/MANAPTS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << y / x << "\n";
+	}
+	return 0;
+}
