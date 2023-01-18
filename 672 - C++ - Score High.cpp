f87a8cccb4d446f310638abce3570c36cc798eb1
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-18 15:55:49 +0000

    added solution

--- 672 - C++ - Score High.cpp
+++ 672 - C++ - Score High.cpp
@@ -2,3 +2,18 @@
 // Rating - 672
 // https://www.codechef.com/problems/HIGHSCORE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, a, b, c, d;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    cin >> a >> b >> c >> d;
+	    int aList[4] = {a, b, c, d};
+	    sort(aList, aList + 4);
+	    cout << aList[3] << "\n";
+	}
+	return 0;
+}
