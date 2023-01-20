d41a362c75c39147aa7e20b19abc6fa942592628
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-20 10:05:31 +0000

    added solution

--- 588 - C++ - Reverse The Number.cpp
+++ 588 - C++ - Reverse The Number.cpp
@@ -2,3 +2,19 @@
 // Rating - 588
 // https://www.codechef.com/problems/FLOW007
 
+#include<iostream>
+using namespace std;
+int main(){
+    int t, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        int ans = 0;
+        while(n) {
+            ans = ans * 10 + n % 10;
+            n/=10;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
