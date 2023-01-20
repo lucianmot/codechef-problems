f838b3c02fd4c7038edb9c739a82098e89e03adf
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-20 10:24:15 +0000

    added solution

--- 739 - C++ - Count the ACs.cpp
+++ 739 - C++ - Count the ACs.cpp
@@ -2,3 +2,19 @@
 // Rating - 739
 // https://www.codechef.com/problems/ACS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, p;
+	cin >> t;
+	while(t--) {
+	    cin >> p;
+	    int answer100 = p / 100;
+	    p %= 100;
+	    int answer1 = p;
+	    
+	    (answer100 + answer1) > 10 ? cout << -1 << "\n" : cout << answer100 + answer1 << "\n";
+	}
+	return 0;
+}
