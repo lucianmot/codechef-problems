57531a7968953dc4e859ef9085183248dd60f5d7
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-20 23:27:28 +0000

    added solution

--- 738 - C++ - Too many items.cpp
+++ 738 - C++ - Too many items.cpp
@@ -2,3 +2,16 @@
 // Rating - 738
 // https://www.codechef.com/problems/POLYBAGS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = ceil(n / 10.0);
+	    cout << answer << "\n";
+	}
+	return 0;
+}
