eb9aca221d7d10821ad63613ab780faf89f0d9c2
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-21 23:08:01 +0000

    added solution

--- 757 - C++ - Presents for Cheffina.cpp
+++ 757 - C++ - Presents for Cheffina.cpp
@@ -2,3 +2,16 @@
 // Rating - 757
 // https://www.codechef.com/problems/PRESENTS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = n % 5 + (n / 5) * 4;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
