1474959b6acad8dd0c597d62a292f67d3b53057e
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-21 22:57:10 +0000

    added solution

--- 750 - C++ - Valid Triangles.cpp
+++ 750 - C++ - Valid Triangles.cpp
@@ -2,3 +2,15 @@
 // Rating - 750
 // https://www.codechef.com/problems/FLOW013
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    (a + b + c) == 180 ? cout << "YES" << "\n" : cout << "NO" << "\n";
+	}
+	return 0;
+}
