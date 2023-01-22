0fd5b14e4c49ecbdd30f3a1eabbb8c3a5bb70d84
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-22 21:31:30 +0000

    added solution

--- 769 - C++ - Possible Victory.cpp
+++ 769 - C++ - Possible Victory.cpp
@@ -2,3 +2,12 @@
 // Rating - 769
 // https://www.codechef.com/problems/T20MCH
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int r, o, c;
+	cin >> r >> o >> c;
+	r < (20 - o) * 36 + c ? cout << "YES" << "\n" : cout << "NO" << "\n";
+	return 0;
+}
