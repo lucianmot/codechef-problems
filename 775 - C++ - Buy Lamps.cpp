1da8a0375cf01ffc17cc089350459510cfb0a81a
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-22 21:59:14 +0000

    added solution

--- 775 - C++ - Buy Lamps.cpp
+++ 775 - C++ - Buy Lamps.cpp
@@ -2,3 +2,23 @@
 // Rating - 775
 // https://www.codechef.com/problems/BUYLAMP
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, k, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> k >> x >> y;
+	    int answer = 0;
+	    if ( x <= y ) {
+	        answer+= x * n;
+	    } else {
+	        answer+= x * k;
+	        int m = n - k;
+	        answer+= y * m;
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
