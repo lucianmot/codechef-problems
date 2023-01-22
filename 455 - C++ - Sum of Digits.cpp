4a32bbd0f1490934034ebeb8ac5db0412662a42e
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-22 21:51:22 +0000

    added solution

--- 455 - C++ - Sum of Digits.cpp
+++ 455 - C++ - Sum of Digits.cpp
@@ -2,3 +2,21 @@
 // Rating - 455
 // https://www.codechef.com/problems/FLOW006
 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int sum = 0;
+        while(n!=0) {
+            int r = n % 10;
+            sum+=r;
+            n/=10;
+        }
+        cout << sum << "\n";
+    }
+    return 0;
+}
