c762f58cbae70f7ffe090b35eacd0beaa84028fd
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-23 22:58:46 +0000

    added solution

--- 667 - C++ - Turbo Sort.cpp
+++ 667 - C++ - Turbo Sort.cpp
@@ -2,3 +2,17 @@
 // Rating - 667
 // https://www.codechef.com/problems/TSORT
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+        sort(a, a+n);
+        for(int i=0; i<n; i++)
+        cout << a[i] << '\n';
+    return 0;
+}
