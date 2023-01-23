9413447c59b93b8b1de10c6cd1e0828314d618ec
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-23 23:06:58 +0000

    added solution

--- 730 - C++ - Pass or Fail.cpp
+++ 730 - C++ - Pass or Fail.cpp
@@ -2,3 +2,15 @@
 // Rating - 730
 // https://www.codechef.com/problems/PASSORFAIL
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x, p;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x >> p;
+	    x * 3 - (n - x) >= p ? cout << "PASS" << "\n" : cout << "FAIL" << "\n";
+	}
+	return 0;
+}
