9041af40e58b2d376b32689a7c76bb71c9c19547
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-23 23:01:26 +0000

    added solution

--- 746 - C++ - Black cells in a chessboard.cpp
+++ 746 - C++ - Black cells in a chessboard.cpp
@@ -2,3 +2,12 @@
 // Rating - 746
 // https://www.codechef.com/problems/BLACKCEL
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << (n * n) / 2 << "\n";
+	return 0;
+}
