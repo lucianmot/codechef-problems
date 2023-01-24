1c6a03457e95e50f80ba53317bfd652e6e0aa043
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-24 22:31:53 +0000

    added solution

--- 879 - C++ - Different Consecutive Characters.cpp
+++ 879 - C++ - Different Consecutive Characters.cpp
@@ -2,3 +2,23 @@
 // Rating - 879
 // https://www.codechef.com/problems/DIFFCONSEC
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    string s;
+	    cin >> s;
+	    int answer = 0;
+	    for (int i = 0; i < s.length(); i++) {
+	        if (s[i] == s[i+1]) {
+	            answer++;
+	        }
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
