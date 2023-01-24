8eeb994827e62fb289972a5af1933e8639d070ab
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-24 22:44:37 +0000

    added solution idea from geeks for geeks

--- 1367 - C++ - Minimise LCS.cpp
+++ 1367 - C++ - Minimise LCS.cpp
@@ -2,3 +2,16 @@
 // Rating - 1367
 // https://www.codechef.com/problems/MINLCS
 
+// going to solve tomorrow just writing solution idea
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// create lcs function int takes in 3 params 1 - 1st chars - 2 - 2nd chars - 3 - n lengths of chars
+// if x[n-1] == y[n-1] return  1 + lcs(pass itself)
+// else return max(lcs pass itself n-1, lcs pass itself m-1)
+
+int main() {
+	// your code goes here
+	return 0;
+}
