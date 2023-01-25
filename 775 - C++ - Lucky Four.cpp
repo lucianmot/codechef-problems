9a52f481e699f11257c670d6bbac650a89f8cfdf
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-25 22:49:48 +0000

    added solution

--- 775 - C++ - Lucky Four.cpp
+++ 775 - C++ - Lucky Four.cpp
@@ -2,3 +2,22 @@
 // Rating - 775
 // https://www.codechef.com/problems/LUCKFOUR
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int answer = 0;
+	    for(int i = n; i > 0; i/=10){
+	        if(i % 10 == 4) {
+	            answer++;
+	        }
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
