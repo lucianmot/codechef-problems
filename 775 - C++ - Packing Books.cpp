2080d21bbb08c2dbd119d5c7be68182c864af208
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-25 23:13:25 +0000

    added solution

--- 775 - C++ - Packing Books.cpp
+++ 775 - C++ - Packing Books.cpp
@@ -2,3 +2,16 @@
 // Rating - 775
 // https://www.codechef.com/problems/BOOKPACK
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int answer = ceil(y/float(z)) * x;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
