547bf412aadc1093301342eb15bb738cc5e51373
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-26 21:54:27 +0000

    added solution

--- 777 - C++ - Mutated Minions.cpp
+++ 777 - C++ - Mutated Minions.cpp
@@ -2,3 +2,26 @@
 // Rating - 777
 // https://www.codechef.com/problems/CHN15A
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int t, n, k;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> k;
+	    int answer = 0;
+	    for (int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        num+=10;
+	        if (num % 7 == 0) {
+	            answer++;
+	        }
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
