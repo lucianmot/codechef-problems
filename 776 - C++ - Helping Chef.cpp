75ecd0cef2394828a927a073c3d46315a522486b
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-26 21:36:38 +0000

    added solution

--- 776 - C++ - Helping Chef.cpp
+++ 776 - C++ - Helping Chef.cpp
@@ -2,3 +2,17 @@
 // Rating - 776
 // https://www.codechef.com/problems/FLOW008
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    n < 10 ? cout << "Thanks for helping Chef!" << "\n" : cout << -1 << "\n";
+	}
+	return 0;
+}
