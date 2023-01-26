6a64d6b149ca75ec9832163f18d2155d104cb7c1
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-26 21:42:00 +0000

    added solution

--- 772 - Janmansh and Games.cpp
+++ 772 - Janmansh and Games.cpp
@@ -2,3 +2,15 @@
 // Rating - 772
 // https://www.codechef.com/problems/JGAMES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    (x + y) % 2 == 0 ? cout << "Janmansh" << "\n" : cout << "Jay" << "\n";
+	}
+	return 0;
+}
