2f25094dbc10b35c3c56ab668f499496b2f7fcca
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-27 22:20:02 +0000

    added solution

--- 787 - C++ - Economics Class.cpp
+++ 787 - C++ - Economics Class.cpp
@@ -2,3 +2,29 @@
 // Rating - 787
 // https://www.codechef.com/problems/ECOCLASS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int supplyList[n];
+	    int answer = 0;
+	    for (int i = 0; i < n; i++) {
+	        cin >> supplyList[i];
+	    }
+	    
+	    for (int j = 0; j < n; j++) {
+	        int num;
+	        cin >> num;
+	        if (num == supplyList[j]) {
+	            answer++;
+	        }
+	    }
+	    
+	    cout << answer << "\n";
+	}
+	return 0;
+}
