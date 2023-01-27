28e2f8653a8b9a81b827d89ebbdedc57d901dd91
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-27 22:15:23 +0000

    added solution

--- 778 - C++ - Get Lowest Free.cpp
+++ 778 - C++ - Get Lowest Free.cpp
@@ -2,3 +2,17 @@
 // Rating - 778
 // https://www.codechef.com/problems/SALE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    int smallList[3] = {a, b, c};
+	    sort(smallList, smallList + 3);
+	    cout << smallList[1] + smallList[2] << "\n";
+	}
+	return 0;
+}
