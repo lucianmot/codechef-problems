6e9ca778e582e4240f1dabe4e93cb6af3bb3eda9
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-28 16:10:05 +0000

    added solution

--- 414 - C++ - Profit Increment.cpp
+++ 414 - C++ - Profit Increment.cpp
@@ -2,3 +2,16 @@
 // Rating - 414
 // https://www.codechef.com/problems/PROINC
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int answer = x + (x / 10) - (x - y);
+	    cout << answer << "\n";
+	}
+	return 0;
+}
