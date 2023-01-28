e7f5fdf5cb36caa40f4594ebeb741dac37bdfbf7
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-28 15:58:30 +0000

    added solution

--- 252 - C++ - Saving Taxes.cpp
+++ 252 - C++ - Saving Taxes.cpp
@@ -2,3 +2,15 @@
 // Rating - 252
 // https://www.codechef.com/problems/TAXSAVING
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << x - y << "\n";
+	}
+	return 0;
+}
