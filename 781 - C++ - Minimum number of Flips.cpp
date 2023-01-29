86616801eafcc84bb7685666206f44bbf4793c2f
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-29 22:55:51 +0000

    added solution

--- 781 - C++ - Minimum number of Flips.cpp
+++ 781 - C++ - Minimum number of Flips.cpp
@@ -2,3 +2,26 @@
 // Rating - 781
 // https://www.codechef.com/problems/MINFLIPS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int sum = 0;
+	    for (int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        sum+=num;
+	    }
+	    sum = abs(sum);
+	    if (n % 2 != 0) {
+	        cout << -1 << "\n";
+	    } else {
+	        cout << sum / 2 << "\n";
+	    }
+	}
+	return 0;
+}
