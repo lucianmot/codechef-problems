cf9a0b2715dae86f674d7ab1275aacb3f4c67f1f
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-29 22:22:45 +0000

    added solution

--- 788 - C++ - RCB and PLayoffs.cpp
+++ 788 - C++ - RCB and PLayoffs.cpp
@@ -2,3 +2,16 @@
 // Rating - 788
 // https://www.codechef.com/problems/RCBPLAY
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int answer = x + z * 2;
+	    answer >= y ? cout << "YES" << "\n" : cout << "NO" << "\n";
+	}
+	return 0;
+}
