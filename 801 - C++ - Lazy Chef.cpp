9c6e1dd51e9b9016e9b821fea963feaca6d4b1a5
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-30 22:22:45 +0000

    added solution

--- 801 - C++ - Lazy Chef.cpp
+++ 801 - C++ - Lazy Chef.cpp
@@ -2,3 +2,18 @@
 // Rating - 801
 // https://www.codechef.com/problems/LAZYCHF
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, m, d;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> m >> d;
+	    int a1 = x * m;
+	    int a2 = x + d;
+	    int answer = min(a1, a2);
+	    cout << answer << "\n";
+	}
+	return 0;
+}
