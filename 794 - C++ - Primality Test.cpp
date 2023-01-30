ef8e215420d899ca21926ee818f3ef0a1883588f
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-30 22:15:17 +0000

    added solution

--- 794 - C++ - Primality Test.cpp
+++ 794 - C++ - Primality Test.cpp
@@ -2,3 +2,30 @@
 // Rating - 794
 // https://www.codechef.com/problems/PRB01
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    bool primeState = true;
+	    
+	    if (n == 0 || n == 1) {
+	        primeState = false;
+	    }
+	    
+	    for (int i = 2; i < n/2; i++) {
+	        if ( n % i == 0) {
+	            primeState = false;
+	            break;
+	        }
+	    }
+	    
+	    primeState == true ? cout << "yes" << "\n" : cout << "no" << "\n";
+	}
+	return 0;
+}
