cce1ddcb3fc389a307bddc7e324fd2aff7778ccb
Lucian Mot <lucian.i.mot@gmail.com>
2023-01-31 14:56:49 +0000

    added solution

--- 793 - Degree of Polynomial.cpp
+++ 793 - Degree of Polynomial.cpp
@@ -2,3 +2,23 @@
 // Rating - 793
 // https://www.codechef.com/problems/DPOLY
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = 0;
+	    for (int i = 0; i < n; ++i) {
+	        int num;
+	        cin >> num;
+	        if (i != 0 && num != 0) {
+	            answer = i;
+	        }
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
