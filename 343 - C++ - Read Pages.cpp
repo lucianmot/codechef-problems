143f64b95ec7a41cad686170ef36944e6cd0c79e
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-01 23:09:52 +0000

    added solution from contest

--- 343 - C++ - Read Pages.cpp
+++ 343 - C++ - Read Pages.cpp
@@ -2,3 +2,16 @@
 // Rating - 343
 // https://www.codechef.com/problems/READPAGES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x >> y;
+	    if ( n <= x * y ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
