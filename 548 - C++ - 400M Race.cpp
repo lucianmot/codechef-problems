2063d61631dd7d721d2cb8ea5f6305727d78df68
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-01 23:11:54 +0000

    added problem from contest

--- 548 - C++ - 400M Race.cpp
+++ 548 - C++ - 400M Race.cpp
@@ -2,3 +2,17 @@
 // Rating - 548
 // https://www.codechef.com/problems/RACE400M
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    if ( x < y && x < z ) cout << "ALICE" << "\n";
+	    else if ( y < x && y < z ) cout << "BOB" << "\n";
+	    else if ( z < x && z < y ) cout << "CHARLIE" << "\n";
+	}
+	return 0;
+}
