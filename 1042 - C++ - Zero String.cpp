d3a911344e68e83fc93fe877ae2554f2ece576e6
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-02 00:31:13 +0000

    added solution contest

--- 1042 - C++ - Zero String.cpp
+++ 1042 - C++ - Zero String.cpp
@@ -2,3 +2,17 @@
 // Rating - 1042
 // https://www.codechef.com/problems/ZEROSTRING
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n, ones;
+    string s;
+    cin >> t;
+    while (t--) {
+        cin >> n >> s;
+        ones = count(s.begin(), s.end(), '1');
+        cout << min(ones, (n - ones + 1)) << "\n";
+    }
+    return 0;
+}
