13db8ec60db6b9fde8d8bac205d6a196c913fbf8
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-02 00:29:43 +0000

    added solution contest

--- 842 - C++ - Redistributing Chocolates.cpp
+++ 842 - C++ - Redistributing Chocolates.cpp
@@ -2,3 +2,17 @@
 // Rating - 842
 // https://www.codechef.com/problems/REDSTRBTN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int elSum = x + y + z;
+	    if ( elSum == 3 || elSum == 4 || elSum == 5 ) cout << "NO" << "\n";
+	    else cout << "YES" << "\n";
+	}
+	return 0;
+}
