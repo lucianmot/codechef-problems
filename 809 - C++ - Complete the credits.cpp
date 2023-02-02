baae72f89e6eea1df5af79bf4291948a048eea27
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-02 21:45:46 +0000

    added solution

--- 809 - C++ - Complete the credits.cpp
+++ 809 - C++ - Complete the credits.cpp
@@ -2,3 +2,21 @@
 // Ratin - 809
 // https://www.codechef.com/problems/CREDITS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if ( x > 65 ) {
+	        cout << "Overload" << "\n";
+	    } else if ( x < 35 ) {
+	        cout << "Underload" << "\n";
+	    } else {
+	        cout << "Normal" << "\n";
+	    }
+	}
+	return 0;
+}
