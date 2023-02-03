6679c06b51754643e260cc8e882c72c34f11c917
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-03 22:57:27 +0000

    added solution

--- 809 - C++ - Cars and Bikes.cpp
+++ 809 - C++ - Cars and Bikes.cpp
@@ -2,3 +2,17 @@
 // Rating - 809
 // https://www.codechef.com/problems/TYRES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int answer = n % 4;
+	    if ( answer > 1 ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
