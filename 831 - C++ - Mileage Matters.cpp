0f94721d7e59294eb55fd8219a8fbdf61a646ba8
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-05 22:19:15 +0000

    added solution

--- 831 - C++ - Mileage Matters.cpp
+++ 831 - C++ - Mileage Matters.cpp
@@ -2,3 +2,19 @@
 // Rating - 831
 // https://www.codechef.com/problems/MILEAGE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x, y, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x >> y >> a >> b;
+	    float petrol = (n / float(a)) * x;
+	    float diesel = (n / float(b)) * y;
+	    if ( petrol == diesel ) cout << "ANY" << "\n";
+	    else if ( petrol < diesel ) cout << "PETROL" << "\n";
+	    else if ( petrol > diesel ) cout << "DIESEL" << "\n";
+	}
+	return 0;
+}
