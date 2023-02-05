76d27937e74f676de3cd43ba6907ca793931dc8e
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-05 22:08:27 +0000

    added solution

--- 648 - C++ - Small Factorial.cpp
+++ 648 - C++ - Small Factorial.cpp
@@ -2,3 +2,21 @@
 // Rating - 648
 // https://www.codechef.com/problems/FCTRL2
 
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iostream>
+using namespace std;
+using namespace boost::multiprecision;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    int n;
+	    cin >> n;
+	    cpp_int fact=1;
+	    for(int i=n; i > 0; i--) fact*=i;
+	    cout << fact << "\n"; 
+	}
+	return 0;
+}
