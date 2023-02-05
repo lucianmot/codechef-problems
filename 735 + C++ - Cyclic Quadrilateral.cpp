b93350cd217dbfe326093a7e40b83aadeefecc57
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-05 22:06:26 +0000

    added solution

--- 735 + C++ - Cyclic Quadrilateral.cpp
+++ 735 + C++ - Cyclic Quadrilateral.cpp
@@ -2,3 +2,16 @@
 // Rating - 735
 // https://www.codechef.com/problems/CYCLICQD
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, d;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c >> d;
+	    if ( a + c == 180 && b + d == 180 ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
