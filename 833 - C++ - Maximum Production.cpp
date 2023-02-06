ca1fba7863d1b9c19b0a3e734d02c2f682719cb4
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-06 19:57:35 +0000

    added solution

--- 833 - C++ - Maximum Production.cpp
+++ 833 - C++ - Maximum Production.cpp
@@ -2,3 +2,16 @@
 // Rating - 833
 // https://www.codechef.com/problems/EITA
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, d, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> d >> x >> y >> z;
+	    int answer = max((x * 7), (y * d + z * (7 - d)));
+	    cout << answer << "\n";
+	}
+	return 0;
+}
