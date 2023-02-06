d8e51489c1e32bc930ce14a0eb167ade990ea67f
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-06 20:27:16 +0000

    added solution

--- 822 - C++ - Chef and His Fruit Stand.cpp
+++ 822 - C++ - Chef and His Fruit Stand.cpp
@@ -2,3 +2,17 @@
 // Rating - 822
 // https://www.codechef.com/problems/FRUITCHAAT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    x/=2;
+	    int answer = min(x, y);
+	    cout << answer << "\n";
+	}
+	return 0;
+}
