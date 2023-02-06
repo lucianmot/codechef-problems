6843394fd770c385334b3b27cce7e350a5996116
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-06 20:30:52 +0000

    added solution

--- 815 - C++ - Guess the bottom face.cpp
+++ 815 - C++ - Guess the bottom face.cpp
@@ -2,3 +2,15 @@
 // Rating - 815
 // https://www.codechef.com/problems/BOTTOM
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    cout << 7 - x << "\n";
+	}
+	return 0;
+ }
