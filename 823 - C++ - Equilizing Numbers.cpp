1ad994ea7227a54585841d74ab4c63abd2a2ebcc
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-07 21:11:18 +0000

    added solution

--- 823 - C++ - Equilizing Numbers.cpp
+++ 823 - C++ - Equilizing Numbers.cpp
@@ -2,3 +2,17 @@
 // Rating - 823
 // https://www.codechef.com/problems/EQLZING
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b;
+	    int answer = (a + b) % 2;
+	    if ( answer == 0 ) cout << "Yes" << "\n";
+	    else cout << "No" << "\n";
+	}
+	return 0;
+}
