695ed0f83f931a755c20ea0660611958bc220da8
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-07 21:09:51 +0000

    added solution

--- 835 - C++ - Good Weather.cpp
+++ 835 - C++ - Good Weather.cpp
@@ -2,3 +2,23 @@
 // Rating - 835
 // https://www.codechef.com/problems/GOODWEAT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a1, a2, a3, a4, a5, a6, a7;
+	cin >> t;
+	while(t--) {
+	    cin >> a1 >> a2 >> a3 >> a4 >> a5 >> a6 >> a7;
+	    int weatherWeek[7] = {a1, a2, a3, a4, a5, a6, a7};
+	    int sunnyDays = 0;
+	    for (int x : weatherWeek) {
+	        if ( x == 1 ) {
+	            sunnyDays++;
+	        }
+	    }
+	    if ( sunnyDays >= 4 ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
