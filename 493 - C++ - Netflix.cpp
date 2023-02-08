8afa03e4359cec34d84452778f2bd942c2b4f6b4
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-08 23:37:25 +0000

    added solution

--- 493 - C++ - Netflix.cpp
+++ 493 - C++ - Netflix.cpp
@@ -2,3 +2,16 @@
 // Rating - 493
 //https://www.codechef.com/problems/NETFLIX
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, x;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c >> x;
+	    if ( (a+b) >= x || (a+c) >= x || (b+c) >= x ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
