596e29534e297b96dcd9417a017e70ae684d9219
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-08 23:33:48 +0000

    added solution

--- 296 - C++ - Battery Health.cpp
+++ 296 - C++ - Battery Health.cpp
@@ -2,3 +2,16 @@
 // Rating - 296
 // https://www.codechef.com/problems/BTRYHLTH
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if (x > 79) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
