1afb95b397461341822f992d413eb090bda4fc12
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-09 14:53:03 +0000

    added solution

--- 823 - C++ - The Preparations.cpp
+++ 823 - C++ - The Preparations.cpp
@@ -2,3 +2,16 @@
 // Rating - 823
 // https://www.codechef.com/problems/SUPCHEF
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, m, n, k;
+	cin >> t;
+	while(t--) {
+	    cin >> m >> n >> k;
+	    if ( m > n * k ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
