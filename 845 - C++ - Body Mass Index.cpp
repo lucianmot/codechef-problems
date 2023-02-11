3f0f527da8ae876466f2d5795458e17f76415c36
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-11 20:56:31 +0000

    added solution

--- 845 - C++ - Body Mass Index.cpp
+++ 845 - C++ - Body Mass Index.cpp
@@ -2,3 +2,24 @@
 // Rating - 845
 // https://www.codechef.com/problems/BMI
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, m, h;
+    cin >> t;
+    while(t--) {
+        cin >> m >> h;
+        int bmi = m / (h * h);
+        if (bmi <= 18) {
+            cout << 1 << "\n";
+        } else if (bmi >= 30) {
+            cout << 4 << "\n";
+        } else if (bmi >= 25) {
+            cout << 3 << "\n";
+        } else if (bmi >= 19) {
+            cout << 2 << "\n";
+        }
+    }
+	return 0;
+}
