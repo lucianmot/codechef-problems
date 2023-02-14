57a3c22b7f87d25dbd1b5b0bc4e4c80febc3d216
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-14 23:15:59 +0000

    added solution

--- 852 - C++ - Summer Heat.cpp
+++ 852 - C++ - Summer Heat.cpp
@@ -2,3 +2,15 @@
 // Rating - 852
 // https://www.codechef.com/problems/COCONUT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, xa, xb, ya, yb;
+	cin >> t;
+	while(t--) {
+	    cin >> xa >> xb >> ya >> yb;
+	    cout << ya / xa + yb / xb << "\n";
+	}
+	return 0;
+}
