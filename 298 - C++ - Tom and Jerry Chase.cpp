5461a2a3b30dca8f978b34a73f41aa4455efac95
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-17 22:44:10 +0000

    added solution

--- 298 - C++ - Tom and Jerry Chase.cpp
+++ 298 - C++ - Tom and Jerry Chase.cpp
@@ -2,3 +2,16 @@
 // Rating - 298
 // https://www.codechef.com/problems/JERRYCHASE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    if ( x < y ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
