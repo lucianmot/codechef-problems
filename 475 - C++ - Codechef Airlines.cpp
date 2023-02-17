ee3a4839620533c6d0485cb172610df987a12398
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-17 22:49:18 +0000

    added solution

--- 475 - C++ - Codechef Airlines.cpp
+++ 475 - C++ - Codechef Airlines.cpp
@@ -2,3 +2,16 @@
 // Rating - 475
 // https://www.codechef.com/problems/AIRLINES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int seats = min((10 * x), y);
+	    cout << seats * z << "\n";
+	}
+	return 0;
+}
