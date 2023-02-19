c2325fc38ec1a9279195c656d283653c66a8d1c9
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-19 18:59:36 +0000

    added solution

--- 857 - C++ - Chefland Visa.cpp
+++ 857 - C++ - Chefland Visa.cpp
@@ -2,3 +2,16 @@
 // Rating - 857
 // https://www.codechef.com/problems/VISA
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x1, x2, y1, y2, z1, z2;
+	cin >> t;
+	while(t--) {
+	    cin >> x1 >> x2 >> y1 >> y2 >> z1 >> z2;
+	    if((x1 <= x2) && (y1 <= y2) && (z1 >= z2)) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
