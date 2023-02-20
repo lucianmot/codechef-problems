47fad313ad3efca9397d72d64c5f2971cdfe661e
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-20 22:18:20 +0000

    added solution

--- 857 - C++ - Devendra and Water Sports.cpp
+++ 857 - C++ - Devendra and Water Sports.cpp
@@ -2,3 +2,17 @@
 // Rating - 859
 // https://www.codechef.com/problems/DEVSPORTS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, z, y, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> z >> y >> a >> b >> c;
+	    int cost = y + a + b + c;
+	    if ( z >= cost ) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
