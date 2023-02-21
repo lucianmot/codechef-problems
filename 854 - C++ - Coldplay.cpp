df2f59f6e27efeedb5048af6f34d7ff85096b60a
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-21 21:45:41 +0000

    added solution

--- 854 - C++ - Coldplay.cpp
+++ 854 - C++ - Coldplay.cpp
@@ -2,3 +2,15 @@
 // Rating - 854
 // https://www.codechef.com/problems/SLOOP
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, m, s;
+	cin >> t;
+	while(t--) {
+	    cin >> m >> s;
+	    cout << m / s << "\n";
+	}
+	return 0;
+}
