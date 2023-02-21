96a2a7886b5365e36393402abab3ed7ad06e58d8
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-21 22:09:59 +0000

    added solution

--- 867 - C++ - Which Divison.cpp
+++ 867 - C++ - Which Divison.cpp
@@ -2,3 +2,17 @@
 // Rating - 867
 // https://www.codechef.com/problems/WHICHDIV
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, r;
+	cin >> t;
+	while(t--) {
+	    cin >> r;
+	    if (r < 1600) cout << 3 << "\n";
+	    else if (r < 2000) cout << 2 << "\n";
+	    else if (r >= 2000) cout << 1 << "\n";
+	}
+	return 0;
+}
