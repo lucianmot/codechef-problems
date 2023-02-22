32feecf4c7c962f7bd06e7600d4c1f48da6d66e5
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-22 21:47:55 +0000

    added solution

--- 238 - C++ - Good Turn.cpp
+++ 238 - C++ - Good Turn.cpp
@@ -2,3 +2,16 @@
 // Rating - 238
 // https://www.codechef.com/problems/GDTURN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    if(x + y > 6) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
