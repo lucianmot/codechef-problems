769b2642d79196bb39293c8dd4b7961c6c4fbdb8
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-22 22:01:28 +0000

    added solution

--- 429 - C++ - Candy Store.cpp
+++ 429 - C++ - Candy Store.cpp
@@ -2,3 +2,18 @@
 // Rating - 429
 // https://www.codechef.com/problems/CANDYSTORE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int answer;
+	    if(y > x) answer = x + (y - x) * 2;
+	    else answer = y;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
