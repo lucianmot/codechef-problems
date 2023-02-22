66a182996ad53f542baaada4268815ee23d3a827
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-22 22:08:21 +0000

    added solution design

--- 855 - C++ - Passing Marks.cpp
+++ 855 - C++ - Passing Marks.cpp
@@ -2,3 +2,13 @@
 // Ratings - 855
 // https://www.codechef.com/problems/CUTOFF
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// take in all numbers
+	// loop take in array
+	// sort array
+	// index x - n and minus 1 the value
+	return 0;
+}
