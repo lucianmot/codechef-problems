242f8e3bd052a3475506ee7abea800f3c9a135ee
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-26 09:11:29 +0000

    added solution

--- 855 - C++ - Chef and Vacation Transportation.cpp
+++ 855 - C++ - Chef and Vacation Transportation.cpp
@@ -2,3 +2,18 @@
 // Rating - 855
 // https://www.codechef.com/problems/CHEFTRANS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    long long int d = x + y;
+	    if(d == z) cout << "EQUAL" << "\n";
+	    else if(d > z) cout << "TRAIN" << "\n";
+	    else if(d < z) cout << "PLANEBUS" << "\n";
+	}
+	return 0;
+}
