68d1c47e4fe2377e48dc677b80d40d205446b9b4
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-26 09:23:52 +0000

    added solution

--- 872 - C++ - Relativity.cpp
+++ 872 - C++ - Relativity.cpp
@@ -2,3 +2,16 @@
 // Ratings - 872
 // https://www.codechef.com/problems/RELATIVE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, g, c;
+	cin >> t;
+	while(t--) {
+	    cin >> g >> c;
+	    int answer = (c * c) / (2 * g);
+	    cout << answer << "\n";
+	}
+	return 0;
+}
