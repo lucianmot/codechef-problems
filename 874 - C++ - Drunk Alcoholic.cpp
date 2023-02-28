21c0551f532ffec1e2e83679b57874ce012ced78
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-28 23:20:25 +0000

    added solution

--- 874 - C++ - Drunk Alcoholic.cpp
+++ 874 - C++ - Drunk Alcoholic.cpp
@@ -2,3 +2,23 @@
 // Rating - 874
 // https://www.codechef.com/problems/DRUNKALK
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, k;
+	cin >> t;
+	while(t--) {
+	    cin >> k;
+	    int answer = 0;
+	    for(int i = 0; i < k; i++) {
+	        if(i % 2 == 0) {
+	            answer+=3;
+	        } else {
+	            answer-=1;
+	        }
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
