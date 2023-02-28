1d1ac1665a6e3b934acc09ae7310fcc46d7f91be
Lucian Mot <lucian.i.mot@gmail.com>
2023-02-28 23:28:24 +0000

    added solution

--- 880 - C++ - Gold Mining.cpp
+++ 880 - C++ - Gold Mining.cpp
@@ -2,3 +2,17 @@
 // Rating - 880
 // https://www.codechef.com/problems/CARRYGOLD
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x >> y;
+	    int answer = (n + 1) * y;
+	    if (answer >= x) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
