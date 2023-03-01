b320cd81df4523fc869ff068b1426cb6120fd43f
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-01 22:30:04 +0000

    added solution

--- 889 - C++ - Chef and Profits.cpp
+++ 889 - C++ - Chef and Profits.cpp
@@ -2,3 +2,16 @@
 // Rating - 889
 // https://www.codechef.com/problems/CHFPROFIT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    int answer = (z - y) * x;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
