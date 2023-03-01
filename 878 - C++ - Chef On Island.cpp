7996c6aaadef63d7f853ee72d51ea55ee29db141
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-01 10:18:22 +0000

    added solution

--- 878 - C++ - Chef On Island.cpp
+++ 878 - C++ - Chef On Island.cpp
@@ -2,3 +2,20 @@
 // Rating - 878
 // https://www.codechef.com/problems/CCISLAND
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	float x, y, xr, yr, d;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> xr >> d;
+	    float food = x / xr;
+	    float water = y / yr;
+	    float answer = min(food, water);
+	    if(answer >= d) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
