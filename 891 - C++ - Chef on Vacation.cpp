4c54a838b879420018cd445d1aad3be900ec2ca6
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-03 22:58:59 +0000

    added solution

--- 891 - C++ - Chef on Vacation.cpp
+++ 891 - C++ - Chef on Vacation.cpp
@@ -2,3 +2,17 @@
 // Rating - 891
 // https://www.codechef.com/problems/CHEFVACATION
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> z;
+	    x+=y;
+	    if(x > z) cout << "NO" << "\n";
+	    else cout << "YES" << "\n";
+	}
+	return 0;
+}
