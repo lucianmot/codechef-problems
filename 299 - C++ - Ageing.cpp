ac8e3ef4285c16639fea89ffe70e3b5fe685eafb
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-03 23:03:01 +0000

    added solution

--- 299 - C++ - Ageing.cpp
+++ 299 - C++ - Ageing.cpp
@@ -2,3 +2,16 @@
 // Rating - 299
 // https://www.codechef.com/problems/AGEING
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    x-=10;
+	    cout << x << "\n"; 
+	}
+	return 0;
+}
