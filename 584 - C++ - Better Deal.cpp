a59cd013cfd4c7bf74276a23fc2d915ea67425e4
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-03 23:11:15 +0000

    added solution

--- 584 - C++ - Better Deal.cpp
+++ 584 - C++ - Better Deal.cpp
@@ -2,3 +2,23 @@
 // Rating - 584
 // https://www.codechef.com/problems/BETDEAL
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b;
+	    a = 100 * (100 - a);
+	    b = 200 * (100 - b);
+	    if(a == b) {
+	        cout << "BOTH" << "\n";
+	    } else if(a < b) {
+	        cout << "FIRST" << "\n";
+	    } else if(a > b) {
+	        cout << "SECOND" << "\n";
+	    }
+	}
+	return 0;
+}
