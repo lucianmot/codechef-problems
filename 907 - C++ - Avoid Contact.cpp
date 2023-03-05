5e142c30ebdfbbcbbaeb87f02ce827d4eb6ca771
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-05 21:45:40 +0000

    added solution

--- 907 - C++ - Avoid Contact.cpp
+++ 907 - C++ - Avoid Contact.cpp
@@ -2,3 +2,17 @@
 // Rating - 907
 // https://www.codechef.com/problems/AVOIDCONTACT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int answer = y * 2 + x - y;
+	    if(x == y) answer-=1;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
