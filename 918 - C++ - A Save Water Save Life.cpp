0b88c1ff35259143c959659d1642ac049d97baf2
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-05 21:54:57 +0000

    added solution

--- 918 - C++ - A Save Water Save Life.cpp
+++ 918 - C++ - A Save Water Save Life.cpp
@@ -2,3 +2,17 @@
 // Rating - 918
 // https://www.codechef.com/problems/SAVWATER
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, h, x, y, c;
+	cin >> t;
+	while(t--) {
+	    cin >> h >> x >> y >> c;
+	    int answer = (x + (y / 2)) * h;
+	    if (answer > c) cout << "NO" << "\n";
+	    else cout << "YES" << "\n";
+	}
+	return 0;
+}
