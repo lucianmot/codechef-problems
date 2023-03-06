f170e0101da2f82b954b5d31e311e9e14eacc0e6
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-06 22:25:59 +0000

    added solution

--- 898 - C++ - Peaceful Party.cpp
+++ 898 - C++ - Peaceful Party.cpp
@@ -2,3 +2,17 @@
 // Rating - 898
 // https://www.codechef.com/problems/MAYOR_PARTY
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    a+=c;
+	    if(a > b) cout << a << "\n";
+	    else cout << b << "\n";
+	}
+	return 0;
+}
