ce8c5e461cbfed162ab6d8128833f06cd57faf53
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-06 22:17:37 +0000

    added solution

--- 841 - C++ - Which Mixture.cpp
+++ 841 - C++ - Which Mixture.cpp
@@ -2,3 +2,17 @@
 // Rating - 841
 // https://www.codechef.com/problems/MIXTURE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b;
+	    if(a > 0 && b > 0) cout << "Solution" << "\n";
+	    else if(b == 0) cout << "Solid" << "\n";
+	    else if(a == 0) cout << "Liquid" << "\n";
+	}
+	return 0;
+}
