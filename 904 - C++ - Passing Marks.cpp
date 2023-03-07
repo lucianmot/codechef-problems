161040dff79e626fbb20b203c305c6c4fdbd7028
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-07 22:03:38 +0000

    added solution

--- 904 - C++ - Passing Marks.cpp
+++ 904 - C++ - Passing Marks.cpp
@@ -2,3 +2,16 @@
 // Rating - 904
 // https://www.codechef.com/problems/PSGRADE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, aMin, bMin, cMin, tMin, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> aMin >> bMin >> cMin >> tMin >> a >> b >> c;
+	    if(a < aMin || b < bMin || c < cMin || (a+b+c) < tMin) cout << "NO" << "\n";
+	    else cout << "YES" << "\n";
+	}
+	return 0;
+}
