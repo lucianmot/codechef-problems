5fd365d9287d7c3aae0b5c665a0a5835c9a25ec4
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-08 21:31:51 +0000

    added solution

--- 320 - C++ - Just One More Episode.cpp
+++ 320 - C++ - Just One More Episode.cpp
@@ -2,3 +2,16 @@
 // Rating - 320
 // https://www.codechef.com/problems/ONEMORE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if(x > 24) cout << "Yes" << "\n";
+	    else cout << "No" << "\n";
+	}
+	return 0;
+}
