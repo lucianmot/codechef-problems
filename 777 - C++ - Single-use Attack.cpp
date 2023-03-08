6bee8b341cec1f9b31bf6c0e2f95658aeef909ee
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-08 21:42:27 +0000

    added solution

--- 777 - C++ - Single-use Attack.cpp
+++ 777 - C++ - Single-use Attack.cpp
@@ -2,3 +2,21 @@
 // Rating - 777
 // https://www.codechef.com/problems/SINGLEUSE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, h, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> h >> x >> y;
+	    h-=y;
+	    int answer = 1;
+	    if(h > 0) {
+	        answer+=(h/x);
+	        if(h % x != 0) answer+=1; 
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
