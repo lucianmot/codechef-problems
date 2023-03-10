a6408433a78c50e51c47e0181059aa06bc6649b7
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-10 10:33:58 +0000

    added solution

--- 895 - C++ - Chef and SnackDown.cpp
+++ 895 - C++ - Chef and SnackDown.cpp
@@ -2,3 +2,21 @@
 // Rating - 895
 // https://www.codechef.com/problems/SNCKYEAR
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	int hosted[5] = {2010, 2015, 2016, 2017, 2019};
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    int answer = 0;
+	    for(int i : hosted) {
+	        if(x == i) answer = 1;
+	    }
+	    if(answer == 0) cout << "NOT HOSTED" << "\n";
+	    else cout << "HOSTED" << "\n";
+	}
+	return 0;
+}
