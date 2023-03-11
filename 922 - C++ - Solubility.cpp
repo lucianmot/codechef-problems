a5439c59703c9ffe6b1fae8b6841e5ca1c3ab048
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-11 21:56:43 +0000

    added solution

--- 922 - C++ - Solubility.cpp
+++ 922 - C++ - Solubility.cpp
@@ -2,3 +2,16 @@
 // Rating - 922
 // https://www.codechef.com/problems/SOLBLTY
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, a, b;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> a >> b;
+	    int answer = (a + (100 - x) * b) * 10;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
