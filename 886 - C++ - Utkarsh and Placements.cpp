95230ed474316d3e68532b0acc66767b841f6ad5
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-11 21:50:12 +0000

    added solution

--- 886 - C++ - Utkarsh and Placements.cpp
+++ 886 - C++ - Utkarsh and Placements.cpp
@@ -2,3 +2,25 @@
 // Rating - 886
 // https://www.codechef.com/problems/UTKPLC
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	char one, two, three, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> one >> two >> three;
+	    cin >> x >> y;
+	    if(x == one) {
+	        cout << x << "\n";
+	    } else if (y == one) {
+	        cout << y << "\n";
+	    } else if (x == two) {
+	        cout << x << "\n";
+	    } else {
+	        cout << y << "\n";
+	    }
+	}
+	return 0;
+}
