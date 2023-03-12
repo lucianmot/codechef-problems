6e63e08f19938b0cd6f84a3ea0877f5803278bd6
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-12 21:55:42 +0000

    added solution

--- 1425 - C++ - Say No To Drugs.cpp
+++ 1425 - C++ - Say No To Drugs.cpp
@@ -2,3 +2,26 @@
 // Rating - 1425
 // https://www.codechef.com/problems/NODRUGS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, k, l, maxSpeed, friendSpeed;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> k >> l;
+	    maxSpeed = 0;
+	    for(int i = 0; i < n - 1; i++) {
+	        int num;
+	        cin >> num;
+	        if(num > maxSpeed) maxSpeed = num;
+	    }
+	    cin >> friendSpeed;
+	    if(k > 0) {
+	        friendSpeed+=(l-1) * k;
+	    }
+	    if(friendSpeed > maxSpeed) cout << "Yes" << "\n";
+	    else cout << "No" << "\n";
+ 	}
+	return 0;
+}
