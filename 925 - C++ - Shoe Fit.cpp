ab83353d7e14175b37648a14df9b7f0113c1a75b
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-13 21:25:27 +0000

    added solution

--- 925 - C++ - Shoe Fit.cpp
+++ 925 - C++ - Shoe Fit.cpp
@@ -2,3 +2,17 @@
 // Rating - 925
 // https://www.codechef.com/problems/SHOEFIT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    int answer = a + b + c;
+	    if(answer < 1 || answer > 2) cout << 0 << "\n";
+	    else cout << 1 << "\n";
+	}
+	return 0;
+}
