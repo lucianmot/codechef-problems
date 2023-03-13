592b0307b80bfe6eca89ed13a6c634a8411e07a4
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-13 21:45:53 +0000

    added solution

--- 931 - C++ - Chef and Price Control.cpp
+++ 931 - C++ - Chef and Price Control.cpp
@@ -2,3 +2,25 @@
 // Rating - 931
 // https://www.codechef.com/problems/PRICECON
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, k;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> k;
+	    int revenue = 0;
+	    int priceCap = 0;
+	    for(int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        revenue+=num;
+	        if(num > k) priceCap+=k;
+	        else priceCap+=num;
+	    }
+	    int answer = revenue - priceCap;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
