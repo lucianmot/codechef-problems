d6d71706fe98f701c4f91087cd9fc8300aba7dae
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-15 21:02:40 +0000

    added solution

--- C++ - Divides Pages.cpp
+++ C++ - Divides Pages.cpp
@@ -2,3 +2,22 @@
 // Ratings - ---
 // https://www.codechef.com/problems/BOOKPAGES
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int totalPages = 0;
+	    for(int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        totalPages+=num;
+	    }
+	    if(n > 1 && totalPages % 2 == 0) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
