16ec7de7620ef9667c523690fa4aca4ea5885ddb
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-15 20:55:46 +0000

    added solution

--- C++ - Sunny Day.cpp
+++ C++ - Sunny Day.cpp
@@ -2,3 +2,16 @@
 // Rating - ---
 // https://www.codechef.com/problems/SUNNYDAY
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if(x > 24) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
