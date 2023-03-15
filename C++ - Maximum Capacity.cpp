dd15c5af114d1c5870d54ff89b21e1d671adad99
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-15 20:59:39 +0000

    added solution

--- C++ - Maximum Capacity.cpp
+++ C++ - Maximum Capacity.cpp
@@ -2,3 +2,18 @@
 // Rating - ---
 // https://www.codechef.com/problems/MAXCAP
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    int answer = x * y;
+	    if(answer < 501 && x < 9) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	    
+	}
+	return 0;
+}
