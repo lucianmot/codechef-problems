17e14ca6a3b551bb23001b54523060993c21859b
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-16 23:14:50 +0000

    added solution

--- 936 - C++ - Two vs Ten.cpp
+++ 936 - C++ - Two vs Ten.cpp
@@ -2,3 +2,19 @@
 // Rating - 936
 // https://www.codechef.com/problems/TWOVSTEN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if(x % 10 == 0) cout << 0 << "\n";
+	    else if(x % 5 == 0) cout << 1 << "\n";
+	    else cout << -1 << "\n";
+	}
+	return 0;
+}
