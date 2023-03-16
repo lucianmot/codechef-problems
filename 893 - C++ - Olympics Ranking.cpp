a6896a40c3d9ee8cb7f3706e63d8359749b461e7
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-16 23:10:10 +0000

    added solution

--- 893 - C++ - Olympics Ranking.cpp
+++ 893 - C++ - Olympics Ranking.cpp
@@ -2,3 +2,18 @@
 // Rating - 893
 // https://www.codechef.com/problems/OLYRANK
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c, x, y, z;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c >> x >> y >> z;
+	    a+=(b + c);
+	    x+=(y + z);
+	    if(a > x) cout << 1 << "\n";
+	    else cout << 2 << "\n";
+	}
+	return 0;
+}
