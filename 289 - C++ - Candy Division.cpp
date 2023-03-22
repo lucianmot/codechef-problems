00797eb7e5e254617708c80639e36ef06804cbcc
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-22 20:57:45 +0000

    added solution

--- 289 - C++ - Candy Division.cpp
+++ 289 - C++ - Candy Division.cpp
@@ -2,3 +2,16 @@
 // Rating - 289
 // https://www.codechef.com/problems/CANDIVIDE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    if(n % 3 == 0) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
