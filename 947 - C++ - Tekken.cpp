8b9b82b376ca4dd6d697d25049d7d395758fec95
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-22 20:52:18 +0000

    added solution

--- 947 - C++ - Tekken.cpp
+++ 947 - C++ - Tekken.cpp
@@ -2,3 +2,17 @@
 // Rating - 947
 // https://www.codechef.com/problems/TEKKEN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, a, b, c;
+	cin >> t;
+	while(t--) {
+	    cin >> a >> b >> c;
+	    int fight = max(b, c) - min(b, c);
+	    if(a > fight) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
