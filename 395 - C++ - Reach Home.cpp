5b286d122d82f75486a932e37863e7698c2f4f8b
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-24 21:52:49 +0000

    added solution

--- 395 - C++ - Reach Home.cpp
+++ 395 - C++ - Reach Home.cpp
@@ -2,3 +2,17 @@
 // Rating - 395
 // https://www.codechef.com/problems/REACH_HOME
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    x*=5;
+	    if(x >= y) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
