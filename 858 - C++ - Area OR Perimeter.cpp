62052c3c80321ca42a8390f39a566fea3c399837
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-25 21:55:24 +0000

    added solution

--- 858 - C++ - Area OR Perimeter.cpp
+++ 858 - C++ - Area OR Perimeter.cpp
@@ -2,3 +2,18 @@
 // Rating - 858
 // https://www.codechef.com/problems/AREAPERI
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+	int l, b, area, peri;
+	cin >> l >> b;
+	area = l * b;
+	peri = 2 * (l + b);
+	if(area == peri) cout << "Eq" << "\n" << area << "\n";
+	else if(area > peri) cout << "Area" << "\n" << area << "\n";
+	else if(area < peri) cout << "Peri" << "\n" << peri << "\n";
+	return 0;
+}
