e55c0a39720bae3b14ee3e72af329c6e593c96e2
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-25 21:56:52 +0000

    added comments

--- 858 - C++ - Area OR Perimeter.cpp
+++ 858 - C++ - Area OR Perimeter.cpp
@@ -6,14 +6,13 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    
-	int l, b, area, peri;
-	cin >> l >> b;
-	area = l * b;
-	peri = 2 * (l + b);
-	if(area == peri) cout << "Eq" << "\n" << area << "\n";
-	else if(area > peri) cout << "Area" << "\n" << area << "\n";
-	else if(area < peri) cout << "Peri" << "\n" << peri << "\n";
-	return 0;
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); // Synchronize input and output streams
+	int l, b, area, peri; // Declare variables
+	cin >> l >> b; // Take inputs
+	area = l * b; // Calculate area
+	peri = 2 * (l + b); // Calculate perimeter
+	if(area == peri) cout << "Eq" << "\n" << area << "\n"; // Prints 'Eq' if area equals perimeter
+	else if(area > peri) cout << "Area" << "\n" << area << "\n"; // Prints 'Area' if area is greater than perimeter
+	else if(area < peri) cout << "Peri" << "\n" << peri << "\n"; // Prints 'Peri' if area is less than perimeter
+	return 0; // Return 0 to indicate successful execution 
 }
