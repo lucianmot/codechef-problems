53170f97d4174f81d34ae8f6f9fb6204f556b0c4
Lucian Mot <lucian.i.mot@gmail.com>
2023-03-27 22:04:58 +0100

    added solution

--- 994 - C++ - Dark Light.cpp
+++ 994 - C++ - Dark Light.cpp
@@ -2,3 +2,15 @@
 // Rating - 994
 // https://www.codechef.com/problems/DARLIG
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n, k;
+    cin >> t;
+    while(t--) {
+        cin >> n >> k;
+        
+    }
+	return 0;
+}
