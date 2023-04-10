bf3519e8dce20620a6b95a6690ab23198f40c60f
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-10 22:10:00 +0100

    added solution

--- 305 - C++ - Chef and Donation.cpp
+++ 305 - C++ - Chef and Donation.cpp
@@ -2,3 +2,15 @@
 // Rating - 305
 // https://www.codechef.com/problems/DNATION
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    cout << y - x << "\n";
+	}
+	return 0;
+}
