307b641e7ce524225afebe2cc3c8f64f10ac22d4
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-10 22:13:27 +0100

    added solution

--- 347 - C++ - Couple Game.cpp
+++ 347 - C++ - Couple Game.cpp
@@ -2,3 +2,15 @@
 // Rating - 347
 // https://www.codechef.com/problems/COUGAME
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, g, b;
+	cin >> t;
+	while(t--) {
+	    cin >> g >> b;
+	    cout << b - g << "\n";
+	}
+	return 0;
+}
