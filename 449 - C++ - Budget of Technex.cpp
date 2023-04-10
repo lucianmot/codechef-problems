4869dbdd0317ab98cabd9201e839cb2067cc81b1
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-10 22:16:21 +0100

    added solution

---
+++ 449 - C++ - Budget of Technex.cpp
@@ -0,0 +1,16 @@
+// C++ - Budget of Technex
+// Rating - 449
+// https://www.codechef.com/problems/BUDTECH
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, r;
+    cin >> t;
+    while(t--) {
+        cin >> r;
+        cout << r * 100 << "\n";
+    }
+	return 0;
+}
