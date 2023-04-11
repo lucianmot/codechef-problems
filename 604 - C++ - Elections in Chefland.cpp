304261a6275c700336be1ecc978679162f7998d2
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-11 23:14:42 +0100

    added solution

--- 604 - C++ - Elections in Chefland.cpp
+++ 604 - C++ - Elections in Chefland.cpp
@@ -2,3 +2,21 @@
 // Rating - 604
 // https://www.codechef.com/problems/ELECTN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    int answer = 0;
+	    for(int i = 0; i < n; i++) {
+	        int num;
+	        cin >> num;
+	        if(num >= x) answer++;
+	    }
+	    cout << answer << "\n";
+	}
+	return 0;
+}
