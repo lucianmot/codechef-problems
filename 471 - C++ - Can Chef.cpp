e9265952e63af08d1366e79d90d07cd839665354
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-14 22:18:18 +0100

    added solution

--- 471 - C++ - Can Chef.cpp
+++ 471 - C++ - Can Chef.cpp
@@ -2,3 +2,18 @@
 // Rating - 471
 // https://www.codechef.com/problems/CANCHEF
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    x*=15;
+	    y+=y;
+	    if(x < y) cout << "NO" << "\n";
+	    else cout << "YES" << "\n";
+	}
+	return 0;
+}
