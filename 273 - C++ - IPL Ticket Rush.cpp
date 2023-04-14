223e5d06112b2ec63d5c028125966d2dc7944804
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-14 22:11:28 +0100

    added solution

--- 273 - C++ - IPL Ticket Rush.cpp
+++ 273 - C++ - IPL Ticket Rush.cpp
@@ -2,3 +2,16 @@
 // Rating - 273
 // https://www.codechef.com/problems/IPLTRSH
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    if(n > m) cout << n - m << "\n";
+	    else cout << 0 << "\n";
+	}
+	return 0;
+}
