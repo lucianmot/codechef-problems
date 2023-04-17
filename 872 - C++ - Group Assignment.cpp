d5a8f7c320e82983f258f0ec5aff48260545b5a1
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-17 21:20:47 +0100

    added solution

--- 872 - C++ - Group Assignment.cpp
+++ 872 - C++ - Group Assignment.cpp
@@ -2,3 +2,15 @@
 // Rating - 872
 // https://www.codechef.com/problems/GROUPASSGN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    cout << 2 * n - x + 1 << "\n";
+	}
+	return 0;
+}
