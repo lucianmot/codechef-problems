db5c37e74d10cad6de46c9d45b0918c3af15f959
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-18 21:43:09 +0100

    added solution

--- 872 - C++ - Chef and Contest.cpp
+++ 872 - C++ - Chef and Contest.cpp
@@ -2,3 +2,23 @@
 // Rating - 872
 // https://www.codechef.com/problems/CHEFCONTEST
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y, p, q;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y >> p >> q;
+	    int chef = x + p * 10;
+	    int chefina = y + q * 10;
+	    if(chef == chefina) {
+	        cout << "Draw" << "\n";
+	    } else if(chef < chefina) {
+	        cout << "Chef" << "\n";
+	    } else if(chef > chefina) {
+	        cout << "Chefina" << "\n";
+	    }
+	}
+	return 0;
+}
