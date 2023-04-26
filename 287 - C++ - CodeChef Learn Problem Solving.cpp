0aaf912d192f8efc2bb8ac955ec69f47c37d93dc
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-26 11:10:43 +0100

    added solution

--- 287 - C++ - CodeChef Learn Problem Solving.cpp
+++ 287 - C++ - CodeChef Learn Problem Solving.cpp
@@ -2,3 +2,12 @@
 // Rating - 287
 // https://www.codechef.com/problems/CCLEARN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << n * 2 << "\n";
+	return 0;
+}
