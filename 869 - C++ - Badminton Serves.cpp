cd3b097cc3d6fc2634fe6e25d23e285b88d39c0a
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-26 11:03:53 +0100

    added solution

--- 869 - C++ - Badminton Serves.cpp
+++ 869 - C++ - Badminton Serves.cpp
@@ -2,3 +2,15 @@
 // Rating - 869
 // https://www.codechef.com/problems/BADMINTON
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+        cout << n/2 + 1 << "\n";
+	}
+	return 0;
+}
