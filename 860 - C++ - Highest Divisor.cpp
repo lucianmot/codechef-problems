6154908e4b1dc5ac1bdac29435a501b09a75659f
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-26 10:31:09 +0100

    added solution

--- 860 - C++ - Highest Divisor.cpp
+++ 860 - C++ - Highest Divisor.cpp
@@ -2,3 +2,18 @@
 // Rating - 860
 // https://www.codechef.com/problems/HDIVISR
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	for(int i=10; i>=1; i--) {
+	    if(n % i == 0) {
+	        cout << i << "\n";
+	        break;
+	    }
+	}
+	return 0;
+}
