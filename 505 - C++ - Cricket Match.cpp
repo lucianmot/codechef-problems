7deb85ab9c7750f209f4abcc3c1f03ef672753d3
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-26 11:17:13 +0100

    added solution

--- 505 - C++ - Cricket Match.cpp
+++ 505 - C++ - Cricket Match.cpp
@@ -2,3 +2,17 @@
 // Rating - 505
 // https://www.codechef.com/problems/CRICMATCH
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    int answer = 36 * m;
+	    if(answer >= n) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
