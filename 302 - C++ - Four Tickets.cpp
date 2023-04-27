33a20f71d280eb90178883459c7606b51e5a56d2
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-27 16:07:49 +0100

    added solution

--- 302 - C++ - Four Tickets.cpp
+++ 302 - C++ - Four Tickets.cpp
@@ -1,3 +1,18 @@
 // C++ - Four Tickets
 // Rating - 302
 // https://www.codechef.com/problems/FOURTICKETS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+        n*=4;
+        if (n <= 1000) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+	}
+	return 0;
+}
