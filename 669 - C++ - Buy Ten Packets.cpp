7462e62f1182e29bd02e3556f296c58905afe4aa
Lucian Mot <lucian.i.mot@gmail.com>
2023-04-28 08:00:36 +0100

    added solution

--- 669 - C++ - Buy Ten Packets.cpp
+++ 669 - C++ - Buy Ten Packets.cpp
@@ -2,3 +2,19 @@
 // Rating - 669
 // https://www.codechef.com/problems/TENPACKETS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >> t;
+	while(t--) {
+	    cin >> x >> y;
+	    x*=5;
+	    y+=(y/2);
+	    y*=2;
+	    if(x < y) cout << x << "\n";
+	    else cout << y << "\n";
+	}
+	return 0;
+}
