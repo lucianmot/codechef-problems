c59c71cc2c837fce11e76aa129325ad05eb1cf96
Lucian Mot <lucian.i.mot@gmail.com>
2023-05-13 23:18:01 +0100

    added solution

--- 419 - C++ - Parliament.cpp
+++ 419 - C++ - Parliament.cpp
@@ -2,3 +2,17 @@
 // Rating - 419
 // https://www.codechef.com/problems/PARLIAMENT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    int answer = n - x;
+	    if(answer > x) cout << "NO" << "\n";
+	    else cout << "YES" << "\n";
+	}
+	return 0;
+}
