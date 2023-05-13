55c14d670e9d6464af029eaafb2058cfd61e0aeb
Lucian Mot <lucian.i.mot@gmail.com>
2023-05-13 23:09:38 +0100

    added solution

--- 255 - C++ - Masterchef finals.cpp
+++ 255 - C++ - Masterchef finals.cpp
@@ -2,3 +2,16 @@
 // Rating - 255
 // https://www.codechef.com/problems/TOP10
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, x;
+	cin >> t;
+	while(t--) {
+	    cin >> x;
+	    if(x < 11) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
