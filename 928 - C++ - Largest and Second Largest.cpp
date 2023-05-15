619e54932ae65fb543cf989553126111b7a47645
Lucian Mot <lucian.i.mot@gmail.com>
2023-05-15 22:02:56 +0100

    added temp solution

--- 928 - C++ - Largest and Second Largest.cpp
+++ 928 - C++ - Largest and Second Largest.cpp
@@ -2,3 +2,26 @@
 // Rating - 928
 // https://www.codechef.com/problems/LARGESECOND
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	while(t--) {
+	    cin >> n;
+	    int listNum[n];
+	    for(int i = 0; i < n; i++) {
+	        cin >> listNum[i];
+	    }
+	    sort(listNum, listNum + n);
+	    int largestInt = listNum[n-1];
+	    int secondLargestInt = 0;
+	    if(largestInt == listNum[n-2]) {
+	        // to be continued
+	    }
+	    int answer = largestInt + secondLargestInt;
+	    cout << answer << "\n";
+	}
+	return 0;
+}
