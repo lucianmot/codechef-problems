ae5513ba8ce51eb09d5676338721bfd9d843fd2a
Lucian Mot <lucian.i.mot@gmail.com>
2023-05-15 21:46:00 +0100

    added solution

--- 713 - C++ - Online or Offline.cpp
+++ 713 - C++ - Online or Offline.cpp
@@ -2,3 +2,19 @@
 // Rating - 713
 // https://www.codechef.com/problems/FOODPLAN
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	double n, m;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> m;
+	    n*=0.9;
+	    if(n==m) cout << "EITHER" << "\n";
+	    else if(n < m) cout << "ONLINE" << "\n";
+	    else if(n > m) cout << "DINING" << "\n";
+	}
+	return 0;
+}
