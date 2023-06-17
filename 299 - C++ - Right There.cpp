3cd0d6964c05fb358c866eb5eba3612c8daa1fc6
Lucian Mot <lucian.i.mot@gmail.com>
2023-06-17 22:05:22 +0100

    added solution

--- 299 - C++ - Right There.cpp
+++ 299 - C++ - Right There.cpp
@@ -2,3 +2,16 @@
 // Rating - 299
 // https://www.codechef.com/problems/RIGHTTHERE
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    if(n <= x) cout << "YES" << "\n";
+	    else cout << "NO" << "\n";
+	}
+	return 0;
+}
