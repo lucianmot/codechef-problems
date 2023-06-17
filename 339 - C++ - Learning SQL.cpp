f6ffceb39791b962f9a6d62e9e9ad8989e850dc3
Lucian Mot <lucian.i.mot@gmail.com>
2023-06-17 22:15:42 +0100

    added solution

--- 339 - C++ - Learning SQL.cpp
+++ 339 - C++ - Learning SQL.cpp
@@ -2,3 +2,13 @@
 // Rating - 339
 // https://www.codechef.com/problems/LEARNSQL
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int r, c, e;
+	cin >> r >> c >> e;
+	int ans = (r + e) * c;
+	cout << ans << "\n";
+	return 0;
+}
