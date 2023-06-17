db5a7234f0dba3e8f5d21a758b00790dd406f180
Lucian Mot <lucian.i.mot@gmail.com>
2023-06-17 22:37:00 +0100

    added solution

--- 390 - C++ - The Gift.cpp
+++ 390 - C++ - The Gift.cpp
@@ -2,3 +2,13 @@
 // Rating - 390
 // https://www.codechef.com/problems/CS2023_GIFT
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, n, m;
+	cin >> x >> n >> m;
+	if(x + m >= n) cout << "YES" << "\n";
+	else cout << "NO" << "\n";
+	return 0;
+}
