44ef5ddbec59d2a18b0d14f7c9d9d7b8b2ee6f7c
Lucian Mot <lucian.i.mot@gmail.com>
2023-06-18 21:50:59 +0100

    added solution

--- 568 - C++ - 7 Rings.cpp
+++ 568 - C++ - 7 Rings.cpp
@@ -2,3 +2,17 @@
 // Rating - 568
 // https://www.codechef.com/problems/SEVENRINGS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, x;
+	cin >> t;
+	while(t--) {
+	    cin >> n >> x;
+	    int ans = n * x;
+	    if(ans < 100000 && ans > 9999) cout << "Yes" << "\n";
+	    else cout << "No" << "\n";
+	}
+	return 0;
+}
