0dd9d6a5670e5fd4603455201509c0ed35587813
Lucian Mot <lucian.i.mot@gmail.com>
2023-06-18 21:38:54 +0100

    added solution

--- 374 - C++ - Oneful Pairs.cpp
+++ 374 - C++ - Oneful Pairs.cpp
@@ -2,3 +2,14 @@
 // Rating - 374
 // https://www.codechef.com/problems/ONEFULPAIRS
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b, ans;
+	cin >> a >> b;
+	ans = a + b + a * b;
+	if(ans == 111) cout << "Yes" << "\n";
+	else cout << "No" << "\n";
+	return 0;
+}
